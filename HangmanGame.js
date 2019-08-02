e79a84d99791dec65b0ff0c3a212575d511eb2db
AminesCodes <aminebensalem@pursuit.org>
2019-08-02 00:59:23 -0400

    V.2 Continue option for when winning, greater possibilities for a random word

--- HangmanGame.js
+++ HangmanGame.js
@@ -1,217 +1,6 @@
-let allTheWords = [
-    'abruptly',
-    'absurd',
-    'abyss',
-    'affix',
-    'askew',
-    'avenue',
-    'awkward',
-    'axiom',
-    'azure',
-    'bagpipes',
-    'bandwagon',
-    'banjo',
-    'bayou',
-    'beekeeper',
-    'bikini',
-    'blitz',
-    'blizzard',
-    'boggle',
-    'bookworm',
-    'boxcar',
-    'boxful',
-    'buckaroo',
-    'buffalo',
-    'buffoon', 
-    'buxom', 
-    'buzzard', 
-    'buzzing', 
-    'buzzwords', 
-    'caliph', 
-    'cobweb', 
-    'cockiness', 
-    'croquet', 
-    'crypt', 
-    'curacao', 
-    'cycle', 
-    'daiquiri', 
-    'dirndl', 
-    'disavow', 
-    'dizzying', 
-    'duplex', 
-    'dwarves', 
-    'embezzle', 
-    'equip', 
-    'espionage', 
-    'euouae', 
-    'exodus', 
-    'faking', 
-    'fishhook', 
-    'fixable', 
-    'fjord', 
-    'flapjack', 
-    'flopping', 
-    'fluffiness', 
-    'flyby', 
-    'foxglove', 
-    'frazzled', 
-    'frizzled', 
-    'fuchsia', 
-    'funny', 
-    'gabby', 
-    'galaxy', 
-    'galvanize', 
-    'gazebo', 
-    'giaour', 
-    'gizmo', 
-    'glowworm', 
-    'glyph', 
-    'gnarly', 
-    'gnostic', 
-    'gossip', 
-    'grogginess', 
-    'haiku', 
-    'haphazard', 
-    'hyphen', 
-    'iatrogenic', 
-    'icebox', 
-    'injury', 
-    'ivory', 
-    'ivy', 
-    'jackpot', 
-    'jaundice', 
-    'jawbreaker', 
-    'jaywalk', 
-    'jazziest', 
-    'jazzy', 
-    'jelly', 
-    'jigsaw', 
-    'jinx', 
-    'jiujitsu', 
-    'jockey', 
-    'jogging', 
-    'joking', 
-    'jovial', 
-    'joyful', 
-    'juicy', 
-    'jukebox', 
-    'jumbo', 
-    'kayak', 
-    'kazoo', 
-    'keyhole', 
-    'khaki', 
-    'kilobyte', 
-    'kiosk', 
-    'kitsch', 
-    'kiwifruit', 
-    'klutz', 
-    'knapsack', 
-    'larynx', 
-    'lengths', 
-    'lucky', 
-    'luxury', 
-    'lymph', 
-    'marquis', 
-    'matrix', 
-    'megahertz', 
-    'microwave', 
-    'mnemonic', 
-    'mystify', 
-    'naphtha', 
-    'nightclub', 
-    'nowadays', 
-    'numbskull', 
-    'nymph', 
-    'onyx', 
-    'ovary', 
-    'oxidize', 
-    'oxygen', 
-    'pajama', 
-    'peekaboo', 
-    'phlegm', 
-    'pixel', 
-    'pizazz', 
-    'pneumonia', 
-    'polka', 
-    'pshaw', 
-    'psyche', 
-    'puppy', 
-    'puzzling', 
-    'quartz', 
-    'queue', 
-    'quips', 
-    'quixotic', 
-    'quiz', 
-    'quizzes', 
-    'quorum', 
-    'razzmatazz', 
-    'rhubarb', 
-    'rhythm', 
-    'rickshaw', 
-    'schnapps', 
-    'scratch', 
-    'shiv', 
-    'snazzy', 
-    'sphinx', 
-    'spritz', 
-    'squawk', 
-    'staff', 
-    'strength', 
-    'strengths', 
-    'stretch', 
-    'stronghold', 
-    'stymied', 
-    'subway', 
-    'swivel', 
-    'syndrome', 
-    'thriftless', 
-    'thumbscrew', 
-    'topaz', 
-    'transcript', 
-    'transgress', 
-    'transplant', 
-    'triphthong', 
-    'twelfth', 
-    'twelfths', 
-    'unknown', 
-    'unworthy', 
-    'unzip', 
-    'uptown', 
-    'vaporize', 
-    'vixen', 
-    'vodka', 
-    'voodoo', 
-    'vortex', 
-    'voyeurism', 
-    'walkway', 
-    'waltz', 
-    'wave', 
-    'wavy', 
-    'waxy', 
-    'wellspring', 
-    'wheezy', 
-    'whiskey', 
-    'whizzing', 
-    'whomever', 
-    'wimpy', 
-    'witchcraft', 
-    'wizard', 
-    'woozy', 
-    'wristwatch', 
-    'wyvern', 
-    'xylophone', 
-    'yachtsman', 
-    'yippee', 
-    'yoked', 
-    'youthful', 
-    'yummy', 
-    'zephyr', 
-    'zigzag', 
-    'zigzagging', 
-    'zilch', 
-    'zipper', 
-    'zodiac', 
-    'zombie'];
+let listOfWords = "able, about, account, acid, across, addition, adjustment, advertisement, after, again, against, agreement, almost, among, amount, amusement, angle, angry, animal, answer, apparatus, apple, approval, arch, argument, army, attack, attempt, attention, attraction, authority, automatic, awake, baby, back, balance, ball, band, base, basin, basket, bath, beautiful, because, before, behaviour, belief, bell, bent, berry, between, bird, birth, bite, bitter, black, blade, blood, blow, blue, board, boat, body, boiling, bone, book, boot, bottle, brain, brake, branch, brass, bread, breath, brick, bridge, bright, broken, brother, brown, brush, bucket, building, bulb, burn, burst, business, butter, button, cake, camera, canvas, card, care, carriage, cart, cause, certain, chain, chalk, chance, change, cheap, cheese, chemical, chest, chief, chin, church, circle, clean, clear, clock, cloth, cloud, coal, coat, cold, collar, colour, comb, come, comfort, committee, common, company, comparison, competition, complete, complex, condition, connection, conscious, control, cook, copper, copy, cord, cork, cotton, cough, country, cover, crack, credit, crime, cruel, crush, current, curtain, curve, cushion, damage, danger, dark, daughter, dead, dear, death, debt, decision, deep, degree, delicate, dependent, design, desire, destruction, detail, development, different, digestion, direction, dirty, discovery, discussion, disease, disgust, distance, distribution, division, door, doubt, down, drain, drawer, dress, drink, driving, drop, dust, early, earth, east, edge, education, effect, elastic, electric, engine, enough, equal, error, even, event, ever, every, example, exchange, existence, expansion, experience, expert, face, fact, fall, false, family, farm, father, fear, feather, feeble, feeling, female, fertile, fiction, field, fight, finger, fire, first, fish, fixed, flag, flame, flat, flight, floor, flower, fold, food, foolish, foot, force, fork, form, forward, fowl, frame, free, frequent, friend, from, front, fruit, full, future, garden, general, girl, give, glass, glove, goat, gold, good, government, grain, grass, great, green, grey, grip, group, growth, guide, hair, hammer, hand, hanging, happy, harbour, hard, harmony, hate, have, head, healthy, hear, hearing, heart, heat, help, high, history, hole, hollow, hook, hope, horn, horse, hospital, hour, house, humour, idea, important, impulse, increase, industry, insect, instrument, insurance, interest, invention, iron, island, jelly, jewel, join, journey, judge, jump, keep, kettle, kick, kind, kiss, knee, knife, knot, knowledge, land, language, last, late, laugh, lead, leaf, learning, leather, left, letter, level, library, lift, light, like, limit, line, linen, liquid, list, little, living, lock, long, look, loose, loss, loud, love, machine, make, male, manager, mark, market, married, mass, match, material, meal, measure, meat, medical, meeting, memory, metal, middle, military, milk, mind, mine, minute, mist, mixed, money, monkey, month, moon, morning, mother, motion, mountain, mouth, move, much, muscle, music, nail, name, narrow, nation, natural, near, necessary, neck, need, needle, nerve, news, night, noise, normal, north, nose, note, number, observation, offer, office, only, open, operation, opinion, opposite, orange, order, organization, ornament, other, oven, over, owner, page, pain, paint, paper, parallel, parcel, part, past, paste, payment, peace, pencil, person, physical, picture, pipe, place, plane, plant, plate, play, please, pleasure, plough, pocket, point, poison, polish, political, poor, porter, position, possible, potato, powder, power, present, price, print, prison, private, probable, process, produce, profit, property, prose, protest, public, pull, pump, punishment, purpose, push, quality, question, quick, quiet, quite, rail, rain, range, rate, reaction, reading, ready, reason, receipt, record, regret, regular, relation, religion, representative, request, respect, responsible, rest, reward, rhythm, rice, right, ring, river, road, roll, roof, room, root, rough, round, rule, safe, sail, salt, same, sand, scale, school, science, scissors, screw, seat, second, secret, secretary, seed, seem, selection, self, send, sense, separate, serious, servant, shade, shake, shame, sharp, sheep, shelf, ship, shirt, shock, shoe, short, shut, side, sign, silk, silver, simple, sister, size, skin, skirt, sleep, slip, slope, slow, small, smash, smell, smile, smoke, smooth, snake, sneeze, snow, soap, society, sock, soft, solid, some, song, sort, sound, soup, south, space, spade, special, sponge, spoon, spring, square, stage, stamp, star, start, statement, station, steam, steel, stem, step, stick, sticky, stiff, still, stitch, stocking, stomach, stone, stop, store, story, straight, strange, street, stretch, strong, structure, substance, such, sudden, sugar, suggestion, summer, support, surprise, sweet, swim, system, table, tail, take, talk, tall, taste, teaching, tendency, test, than, that, then, theory, there, thick, thin, thing, this, thought, thread, throat, through, through, thumb, thunder, ticket, tight, till, time, tired, together, tomorrow, tongue, tooth, touch, town, trade, train, transport, tray, tree, trick, trouble, trousers, true, turn, twist, umbrella, under, unit, value, verse, very, vessel, view, violent, voice, waiting, walk, wall, warm, wash, waste, watch, water, wave, weather, week, weight, well, west, wheel, when, where, while, whip, whistle, white, wide, will, wind, window, wine, wing, winter, wire, wise, with, woman, wood, wool, word, work, worm, wound, writing, wrong, year, yellow, yesterday, young";
+
+let allTheWords = listOfWords.split(', ');
   
   let step0 = `
     ====|=====||
@@ -294,7 +83,6 @@ let allTheWords = [
   
   let randomWord; // a random word from our array of words
   let tryCounter = 0; // counter for the failed tries
-  let winning; // Boolean to determin if the player won or lose
   let userAnswer; // This will be the user's answer formated
   let displayedWord = ''; // What will be displayed at each step of the game;
   let gameOver = false;
@@ -346,7 +134,7 @@ let allTheWords = [
     randomWord = getRandomWord(allTheWords);
     displayedWord = formatedWord();
   
-    //console.log(randomWord, `\n\n\n`) // this line is for the test purpose, it has to be deleted after
+    console.log(randomWord, `\n\n\n`) // this line is for the test purpose, it has to be deleted after
     console.log(`if you wish to quit at anytime, press "control" and "c" to end the game (^c)\n\n`);
     console.log(`Guess the word, it has `, randomWord.length, ` characters! ... Good luck :) \n`);
     console.log(`Please remember to entre one character at the time\n\nYou have 6 tries\n\n`);
@@ -384,11 +172,7 @@ let allTheWords = [
   })
   
   readLine.on('close', ()=> {
-    if (winning) {
-      console.log(`CONGRATULATION !!! YOU WON`); 
-    } else {
-      console.log(`GAME OVER !!\nTHANK YOU FOR PLAYING`);
-    } 
+      console.log(`\nTHANK YOU FOR PLAYING`);
   })
   
   
@@ -400,12 +184,15 @@ let allTheWords = [
       userAnswer = (userInput.trim()).toLowerCase();
   
       if (charIsPartOfTheWord(userAnswer) && !gameOver) {
-        console.log(`Good guess\n\n`);
-        textPicture();
-        if (!displayedWord.includes('_')) {
-          winning = true;
-          readLine.close();
-        }
+          console.log(`Good guess\n\n`);
+          textPicture();
+          if (!displayedWord.includes('_')) {
+              console.log(`\nCONGRATULATION !!! YOU WON\nIt took you ${randomWord.length + tryCounter} guesses\n`);
+              gameOver = true;
+              userAnswer = 'PAUSE'
+              readLine.setPrompt(`Do you wish to start over ? Y / N ?\n`);
+              readLine.prompt();
+            }
       } else if (!gameOver) {
         if (userAnswer.length > 1) {
           tryCounter++;
@@ -416,14 +203,15 @@ let allTheWords = [
           //textPicture();
         }
         if (tryCounter < 6) {
-          readLine.setPrompt(`${userAnswer.toUpperCase()} is not within the word to guess... Try again \n`);
+          readLine.setPrompt(`\n${userAnswer.toUpperCase()} is not within the word to guess... Try again \n`);
           readLine.prompt();
           textPicture();
         } else { 
           textPicture();
           gameOver = true;
           userAnswer = 'PAUSE'
-          console.log(`GAME OVER !!\n\nThe solution was : ${randomWord.toUpperCase()}\n\n`);
+          console.log(`\n${userAnswer.toUpperCase()} is not within the word to guess
+          GAME OVER !!\n\nThe solution was : ${randomWord.toUpperCase()}\n\n`);
           readLine.setPrompt(`Do you wish to start over ? Y / N ?\n`);
           readLine.prompt();
         }
