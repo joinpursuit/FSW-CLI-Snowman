ddb0aedf25ef512a5dabb71b64cfde695dc3cc5c
Scripter12 <elicardona57@gmail.com>
2019-08-03 11:33:32 -0400

    complete

--- hangman.js
+++ hangman.js
@@ -1,17 +1,61 @@
-const readline = require('readline-sync');
 
-function getValidLetterGuess() {
-  function guessIsValid(letter) {
-    return letter.length === 1 && letter.toUpperCase() != letter.toLowerCase()
-  }
-  let letter = ""
-  while (!letter) {
-    let input = readline.question("Please enter your guess: ")
-    if (guessIsValid(input)) {
-      letter = input
-    } else {
-      console.log("Please enter a valid letter")
-    }
-  }
-  return letter.toLowerCase()
+let allTheWords = require("./words.js")
+
+let answer = allTheWords[Math.floor(Math.random() * allTheWords.length)]
+let correctGuess = []
+for(let i = 0; i < answer.length;i++){
+  correctGuess[i] = "_";
 }
+let guesses = []
+let tries = 0;
+let lives = 6;
+
+const readline = require('readline').createInterface({
+  input: process.stdin,
+  output: process.stdout
+})
+
+function hangman(){
+readline.question(`Guess a letter: `, (playersGuess) => {
+
+tries++
+
+if(playersGuess){
+  playersGuess = playersGuess.toLowerCase()
+  if(guesses.includes(playersGuess)){
+
+
+    console.log("You already guessed that letter.")
+  }else if(answer.includes(playersGuess)){
+for(let i = 0; i < correctGuess.length;i++){
+  if(playersGuess === answer[i]){
+  correctGuess[i] = playersGuess
+}
+}
+guesses.push(playersGuess)
+
+}else{
+  guesses.push(playersGuess)
+  lives--
+ }
+}
+
+console.log(correctGuess.join(""))
+console.log("Guessed letters : " + guesses.join(","))
+console.log(`You have ${lives} guesses remaining.`)
+
+
+if(lives === 0){
+  console.log(`You Lose! The answer was ${answer}`)
+  readline.close()
+}
+else if(correctGuess.join("") === answer){
+  console.log(`You Win! It took you ${tries} guesses`)
+  readline.close()
+}else{
+hangman()
+}
+})
+}
+
+hangman()
