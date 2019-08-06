b97e5e5fac4ac6401f9a2a2cd6c5a6aa5d39fceb
Bpmauge <briahanamauge@pursuit.org>
2019-08-06 18:51:50 -0400

    best update with working code

--- alejo hangman.js
+++ alejo hangman.js
@@ -1,4 +1,57 @@
+const readlineSync = require('readline-sync');
+
 let words = ['daddy', 'bubble', 'button','car'];
+
 const getRandomWord = (words) => {
-  let randomIndex = Math.floor(Math.random() * words.length))
+  let randomIndex = Math.floor(Math.random() * words.length);
+  return words[randomIndex];
 }
+
+
+const createHiddenWord = (word) => {
+  let hiddenWord = word.split('').map(char => {return '_'})
+  return hiddenWord
+}
+
+// const createHiddenWord2 = (word) => {
+//   let hiddenWord = [];
+//   for (i = 0; i < word.length; i++) {
+//     hiddenWord.push('_');
+//   } return hiddenWord
+// }
+const isLetterInWord = (letter, word) => {
+  return word.includes(letter)
+}
+const replaceMatchedLetter = (letter, randomWord, hiddenWord) => {
+  for (let i = 0; i < randomWord.length; i++) {
+    let currentchar = randomWord[i];
+    if (currentchar === letter) {
+      hiddenWord[i] = letter;
+    }
+  }
+  return hiddenWord;
+}
+
+
+ const startGame = () => {
+   let randomWord = getRandomWord(words);
+   console.log(randomWord);
+   let hiddenWord = createHiddenWord(randomWord);
+   console.log(hiddenWord.join(''));
+   let tries = 6;
+
+
+   while(tries > 0 && hiddenWord.join('')  !== randomWord){
+     const answer = readlineSync.question('Type a letter: ');
+     if (isLetterInWord(answer, randomWord)){
+       console.log('yes');
+       hiddenWord = replaceMatchedLetter(answer, randomWord, hiddenWord);
+       console.log(hiddenWord);
+     } else{
+       console.log('no');
+       console.log(`you have ${tries-1} remaining`);
+       tries--;
+     }
+   }
+ }
+ startGame();
--- hangman.js
+++ hangman.js
@@ -1,56 +1,121 @@
 const readline = require('readline-sync');
+const say = require('say');
 
-let allTheWords = 'able, about, account, acid, across, addition, adjustment, advertisement, after, again, against, agreement, almost, among, amount, amusement, angle, angry, animal, answer, apparatus, apple, approval, arch, argument, army, attack, attempt, attention, attraction, authority, automatic, awake, baby, back, balance, ball, band, base, basin, basket, bath, beautiful, because, before, behaviour, belief, bell, bent, berry, between, bird, birth, bite, bitter, black, blade, blood, blow, blue, board, boat, body, boiling, bone, book, boot, bottle, brain, brake, branch, brass, bread, breath, brick, bridge, bright, broken, brother, brown, brush, bucket, building, bulb, burn, burst, business, butter, button, cake, camera, canvas, card, care, carriage, cart, cause, certain, chain, chalk, chance, change, cheap, cheese, cheetah, chemical, chest, chief, chin, church, circle, clean, clear, clock, cloth, cloud, coal, coat, cold, collar, colour, comb, come, comfort, committee, common, company, comparison, competition, complete, complex, condition, connection, conscious, control, cook, copper, copy, cord, cork, cotton, cough, country, cover, crack, credit, crime, cruel, crush, current, curtain, curve, cushion, damage, danger, dark, daughter, dead, dear, death, debt, decision, deep, degree, delicate, dependent, design, desire, destruction, detail, development, different, digestion, direction, dirty, discovery, discussion, disease, disgust, distance, distribution, division, door, doubt, down, drain, drawer, dress, drink, driving, drop, dust, early, earth, east, edge, education, effect, elastic, electric, engine, enough, equal, error, even, event, ever, every, example, exchange, existence, expansion, experience, expert, face, fact, fall, false, family, farm, father, fear, feather, feeble, feeling, female, fertile, fiction, field, fight, finger, fire, first, fish, fixed, flag, flame, flat, flight, floor, flower, fold, food, foolish, foot, force, fork, form, forward, fowl, frame, free, frequent, friend, from, front, fruit, full, future, garden, general, girl, give, glass, glove, goat, gold, good, government, grain, grass, great, green, grey, grip, group, growth, guide, hair, hammer, hand, hanging, happy, harbour, hard, harmony, hate, have, head, healthy, hear, hearing, heart, heat, help, high, history, hole, hollow, hook, hope, horn, horse, hospital, hour, house, humour, idea, important, impulse, increase, industry, insect, instrument, insurance, interest, invention, iron, island, jelly, jewel, join, journey, judge, jump, keep, kettle, kick, kind, kiss, knee, knife, knot, knowledge, land, language, last, late, laugh, lead, leaf, learning, leather, left, letter, level, library, lift, light, like, limit, line, linen, liquid, list, little, living, lock, long, look, loose, loss, loud, love, machine, make, male, manager, mark, market, married, mass, match, material, meal, measure, meat, medical, meeting, memory, metal, middle, military, milk, mind, mine, minute, mist, mixed, money, monkey, month, moon, morning, mother, motion, mountain, mouth, move, much, muscle, music, nail, name, narrow, nation, natural, near, necessary, neck, need, needle, nerve, news, night, noise, normal, north, nose, note, number, observation, offer, office, only, open, operation, opinion, opposite, orange, order, organization, ornament, other, oven, over, owner, page, pain, paint, paper, parallel, parcel, part, past, paste, payment, peace, pencil, person, physical, picture, pipe, place, plane, plant, plate, play, please, pleasure, plough, pocket, point, poison, polish, political, poor, porter, position, possible, potato, powder, power, present, price, print, prison, private, probable, process, produce, profit, property, prose, protest, public, pull, pump, punishment, purpose, push, quality, question, quick, quiet, quite, rail, rain, range, rate, reaction, reading, ready, reason, receipt, record, regret, regular, relation, religion, representative, request, respect, responsible, rest, reward, rhythm, rice, right, ring, river, road, roll, roof, room, root, rough, round, rule, safe, sail, salt, same, sand, scale, school, science, scissors, screw, seat, second, secret, secretary, seed, seem, selection, self, send, sense, separate, serious, servant, shade, shake, shame, sharp, sheep, shelf, ship, shirt, shock, shoe, short, shut, side, sign, silk, silver, simple, sister, size, skin, skirt, sleep, slip, slope, slow, small, smash, smell, smile, smoke, smooth, snake, sneeze, snow, soap, society, sock, soft, solid, some, song, sort, sound, soup, south, space, spade, special, sponge, spoon, spring, square, stage, stamp, star, start, statement, station, steam, steel, stem, step, stick, sticky, stiff, still, stitch, stocking, stomach, stone, stop, store, story, straight, strange, street, stretch, strong, structure, substance, such, sudden, sugar, suggestion, summer, support, surprise, sweet, swim, system, table, tail, take, talk, tall, taste, teaching, tendency, test, than, that, then, theory, there, thick, thin, thing, this, thought, thread, throat, through, through, thumb, thunder, ticket, tight, till, time, tired, together, tomorrow, tongue, tooth, touch, town, trade, train, transport, tray, tree, trick, trouble, trousers, true, turn, twist, umbrella, under, unit, value, verse, very, vessel, view, violent, voice, waiting, walk, wall, warm, wash, waste, watch, water, wave, weather, week, weight, well, west, wheel, when, where, while, whip, whistle, white, wide, will, wind, window, wine, wing, winter, wire, wise, with, woman, wood, wool, word, work, worm, wound, writing, wrong, year, yellow, yesterday, young, zebra, zoo'
+const hangman = () => {
+  let hi1 = [" _    _                                                     "];
+  let hi2 = ["| |  | |                                                    "];
+  let hi3 = ["| |__| |   __ _   _ __     __ _   _ __ ___     __ _   _ __  "];
+  let hi4 = ["|  __  |  / _` | | '_ \\   / _` | | '_ ` _ \\   / _` | | '_ \\ "];
+  let hi5 = ["| |  | | | (_| | | | | | | (_| | | | | | | | | (_| | | | | |"];
+  let hi6 = ["|_|  |_|  \\__,_| |_| |_|  \\__, | |_| |_| |_|  \\__,_| |_| |_|"];
+  let hi7 = ["                           __/ |                            "];
+  let hi8 = ["                          |___/                             "];
 
-let allOfTheWords = allTheWords.split(', ');
-
-let getRandomWord = function (array) {
-  return /*oneWord =*/ allOfTheWords[Math.floor(Math.random() * allOfTheWords.length)];
-};
-
-const selectedWord = getRandomWord(allOfTheWords).split('');
-console.log(selectedWord)
-
-const selectedWordBlanks = [];
-for (let i = 0; i < selectedWord.length; i++) {
-  selectedWordBlanks[i] = '_';
+  console.log(hi1.join());
+  console.log(hi2.join());
+  console.log(hi3.join());
+  console.log(hi4.join());
+  console.log(hi5.join());
+  console.log(hi6.join());
+  console.log(hi7.join());
+  console.log(hi8.join());
 }
 
-let answerArray = selectedWordBlanks;
-console.log(selectedWordBlanks.join(' '));
+const youWin = () => {
+let win1 = ["__  __ ____   __  __   _      __ ____ _  __"];
+let win2 = ["\\ \\/ // __ \\ / / / /  | | /| / //  _// |/ /"];
+let win3 = [" \\  // /_/ // /_/ /   | |/ |/ /_/ / /    / "];
+let win4 = [" /_/ \\____/ \\____/    |__/|__//___//_/|_/  "];
+let win5 = ["                                           "];
 
-let remainingLetters = selectedWord.length;
-let guess = 6;
-let guessedLetters = [];
+console.log(win1.join());
+console.log(win2.join());
+console.log(win3.join());
+console.log(win4.join());
+console.log(win5.join());
+}
 
+const youLose = () => {
+let lose1 = ["__  __ ____   __  __  __   ____   ____ ____"];
+let lose2 = ["\\ \\/ // __ \\ / / / / / /  / __ \\ / __// __/"];
+let lose3 = [" \\  // /_/ // /_/ / / /__/ /_/ /_\\ \\ / _/  "];
+let lose4 = [" /_/ \\____/ \\____/ /____/\\____//___//___/  "];
+let lose5 = ["                                           "];
+
+console.log(lose1.join());
+console.log(lose2.join());
+console.log(lose3.join());
+console.log(lose4.join());
+console.log(lose5.join());
+}
+
+let allTheWords = 'able, about, account, acid, across, addition, adjustment, advertisement, after, again, against, agreement, almost, among, amount, amusement, angle, angry, animal, answer, apparatus, apple, approval, arch, argument, army, attack, attempt, attention, attraction, authority, automatic, awake, baby, back, balance, ball, band, base, basin, basket, bath, beautiful, because, before, behaviour, belief, bell, bent, berry, between, bird, birth, bite, bitter, black, blade, blood, blow, blue, board, boat, body, boiling, bone, book, boot, bottle, brain, brake, branch, brass, bread, breath, brick, bridge, bright, broken, brother, brown, brush, bucket, building, bulb, burn, burst, business, butter, button, cake, camera, canvas, card, care, carriage, cart, cause, certain, chain, chalk, chance, change, cheap, cheese, cheetah, chemical, chest, chief, chin, church, circle, clean, clear, clock, cloth, cloud, coal, coat, cold, collar, colour, comb, come, comfort, committee, common, company, comparison, competition, complete, complex, condition, connection, conscious, control, cook, copper, copy, cord, cork, cotton, cough, country, cover, crack, credit, crime, cruel, crush, current, curtain, curve, cushion, damage, danger, dark, daughter, dead, dear, death, debt, decision, deep, degree, delicate, dependent, design, desire, destruction, detail, development, different, digestion, direction, dirty, discovery, discussion, disease, disgust, distance, distribution, division, door, doubt, down, drain, drawer, dress, drink, driving, drop, dust, early, earth, east, edge, education, effect, elastic, electric, engine, enough, equal, error, even, event, ever, every, example, exchange, existence, expansion, experience, expert, face, fact, fall, false, family, farm, father, fear, feather, feeble, feeling, female, fertile, fiction, field, fight, finger, fire, first, fish, fixed, flag, flame, flat, flight, floor, flower, fold, food, foolish, foot, force, fork, form, forward, fowl, frame, free, frequent, friend, from, front, fruit, full, future, garden, general, girl, give, glass, glove, goat, gold, good, government, grain, grass, great, green, grey, grip, group, growth, guide, hair, hammer, hand, hanging, happy, harbour, hard, harmony, hate, have, head, healthy, hear, hearing, heart, heat, help, high, history, hole, hollow, hook, hope, horn, horse, hospital, hour, house, humour, idea, important, impulse, increase, industry, insect, instrument, insurance, interest, invention, iron, island, jelly, jewel, join, journey, judge, jump, keep, kettle, kick, kind, kiss, knee, knife, knot, knowledge, land, language, last, late, laugh, lead, leaf, learning, leather, left, letter, level, library, lift, light, like, limit, line, linen, liquid, list, little, living, lock, long, look, loose, loss, loud, love, machine, make, male, manager, mark, market, married, mass, match, material, meal, measure, meat, medical, meeting, memory, metal, middle, military, milk, mind, mine, minute, mist, mixed, money, monkey, month, moon, morning, mother, motion, mountain, mouth, move, much, muscle, music, nail, name, narrow, nation, natural, near, necessary, neck, need, needle, nerve, news, night, noise, normal, north, nose, note, number, observation, offer, office, only, open, operation, opinion, opposite, orange, order, organization, ornament, other, oven, over, owner, page, pain, paint, paper, parallel, parcel, part, past, paste, payment, peace, pencil, person, physical, picture, pipe, place, plane, plant, plate, play, please, pleasure, plough, pocket, point, poison, polish, political, poor, porter, position, possible, potato, powder, power, present, price, print, prison, private, probable, process, produce, profit, property, prose, protest, public, pull, pump, punishment, purpose, push, quality, question, quick, quiet, quite, rail, rain, range, rate, reaction, reading, ready, reason, receipt, record, regret, regular, relation, religion, representative, request, respect, responsible, rest, reward, rhythm, rice, right, ring, river, road, roll, roof, room, root, rough, round, rule, safe, sail, salt, same, sand, scale, school, science, scissors, screw, seat, second, secret, secretary, seed, seem, selection, self, send, sense, separate, serious, servant, shade, shake, shame, sharp, sheep, shelf, ship, shirt, shock, shoe, short, shut, side, sign, silk, silver, simple, sister, size, skin, skirt, sleep, slip, slope, slow, small, smash, smell, smile, smoke, smooth, snake, sneeze, snow, soap, society, sock, soft, solid, some, song, sort, sound, soup, south, space, spade, special, sponge, spoon, spring, square, stage, stamp, star, start, statement, station, steam, steel, stem, step, stick, sticky, stiff, still, stitch, stocking, stomach, stone, stop, store, story, straight, strange, street, stretch, strong, structure, substance, such, sudden, sugar, suggestion, summer, support, surprise, sweet, swim, system, table, tail, take, talk, tall, taste, teaching, tendency, test, than, that, then, theory, there, thick, thin, thing, this, thought, thread, throat, through, through, thumb, thunder, ticket, tight, till, time, tired, together, tomorrow, tongue, tooth, touch, town, trade, train, transport, tray, tree, trick, trouble, trousers, true, turn, twist, umbrella, under, unit, value, verse, very, vessel, view, violent, voice, waiting, walk, wall, warm, wash, waste, watch, water, wave, weather, week, weight, well, west, wheel, when, where, while, whip, whistle, white, wide, will, wind, window, wine, wing, winter, wire, wise, with, woman, wood, wool, word, work, worm, wound, writing, wrong, year, yellow, yesterday, young, zebra, zoo'.split(', ')
+
+
+const getRandomWord = (array) => {
+  return allTheWords[Math.floor(Math.random() * allTheWords.length)];
+};
+
+
+const startGame = () => {
+  hangman();
+  const selectedWord = getRandomWord(allTheWords).split('');
+  console.log(selectedWord)
+  const selectedWordBlanks = [];
+  for (let i = 0; i < selectedWord.length; i++) {
+    selectedWordBlanks[i] = '_';
+  }
+  let answerArray = selectedWordBlanks;
+  console.log(selectedWordBlanks.join(' '));
+
+  let remainingLetters = selectedWord.length;
+  let guess = 6;
+  let guessedLetters = [];
+
+say.speak(`Welcome to Hangman! This is a ${selectedWord.length} letter word. Please enter you guess.`)
 for (; remainingLetters > 0 && guess > 0;) {
-  //console.log(`${remainingLetters}`)
-      if(guess > 0) {
-        let input = readline.question('Please enter your guess: ');
+      if(guess > 0) {console.log(`These are your previously guessed letter: ${guessedLetters}`)
+      console.log('');
+      console.log('');
+        let input = readline.question(`You have ${guess} guesses remaining. \n Please enter your guess: `);
         console.log('');
-        if (selectedWord.includes(input)) {
-          for (let k = 0; k < selectedWord.length; k++) {
-            if (selectedWord[k] === input){
-              answerArray[k] = input;
-              remainingLetters--;
+        if (guess === 0) {
+          console.log(`The word was ${selectedWord.join('')}`);
+        } else if (selectedWord.includes(input)) {
+          console.log('');
+           for (let k = 0; k < selectedWord.length; k++) {
+             if (selectedWord[k] === input){
+               answerArray[k] = input;
+               remainingLetters--;
+               say.stop();
+               say.speak(`You got it!`);
+             }
+               for (; remainingLetters === 0; remainingLetters--){
+                 youWin();
+                 say.stop();
+                 say.speak('Congratulations, You win!')
+               }
             }
-          }
-        } else if (guess === 0){
-          console.log(`You have ${guess-1} guesses remaining`);
-          console.log(`Game Over!! The word was ${selectedWord.join('')}`);
-        } else {
-          console.log(`You have ${guess-1} guesses remaining`);
+           console.log(answerArray.join(''));
+         } else {
           console.log('');
           guess--;
           guessedLetters.push(input);
-          // guessedLetters += input;
+          console.log(answerArray.join(''));
+          say.stop();
+          say.speak(`Try again.       You have ${guess} guesses remaining.`)
+            if(guess === 0){
+              console.log('');
+              console.log(`You have ${guess} guess(es) remaining.`);
+              youLose();
+              say.stop();
+              say.speak(`You lost!     The word was ${selectedWord.join('')}.      Better luck next time.`)
+              console.log(`The word was ${selectedWord.join('')}`);
+                                                                                                                                                                                                                                                                                                                                                                                                           }
           }
-        }
-        console.log(answerArray.join(''));
-        console.log('');
-        console.log(`These are your previously guessed letter: ${guessedLetters}`)
-        console.log('');
-      }  //console.log(`Game Over!! The word was ${selectedWord.join('')}`);
+      }
+}
+}
 
 
-//console.log(remainingLetters);
+startGame();
