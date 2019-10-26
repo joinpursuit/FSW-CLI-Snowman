66bd911d9ceb517b23c05e05a29e188a37bb96f6
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2019-10-26 09:06:55 -0400

    create secretWord() function, and revealWord() function

--- ComputerPlayer.js
+++ ComputerPlayer.js
@@ -1,13 +1,22 @@
+const Board = require("./Board.js");
+const HumanPlayer = require("./HumanPlayer.js");
+
 class ComputerPlayer {
     constructor(dictionary) {
         this.dictionary = dictionary;
+        this.newWord = [];
     }
 
     secretWord() {
         let wordIndex = Math.floor(Math.random() * this.dictionary.length - 1);
-        return this.dictionary[word];
+        this.newWord = this.dictionary[wordIndex]
+        return this.newWord;
     }
 
-    
+    revealWord() {
+        Board.board = this.newWord;
+    }
 
 }
+
+module.exports = ComputerPlayer;
