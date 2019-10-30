d44b256ed1b4cf939b6f60a48603e332f19a1175
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2019-10-30 08:09:01 -0400

    add in difficulty functionality to ComputerPlayer.js

--- ComputerPlayer.js
+++ ComputerPlayer.js
@@ -9,9 +9,49 @@ class ComputerPlayer {
         this.difficulty;
     } // End of constructor
 
+    secretWordDiff1() {
+        while(this.newWord.length > 4) {
+            this.newWord = this.dictionary[Math.floor(Math.random() * this.dictionary.length)];
+        }
+    } // End of secretWordDiff1() function
+
+    secretWordDiff2() {
+        while(this.newWord.length > 7 || this.newWord.length < 5) {
+            this.newWord = this.dictionary[Math.floor(Math.random() * this.dictionary.length)];
+        }
+    } // End of secretWordDiff2() function
+
+    secretWordDiff3() {
+        while(this.newWord.length > 10 || this.newWord.length < 8) {
+            this.newWord = this.dictionary[Math.floor(Math.random() * this.dictionary.length)];
+        }
+    } // End of secretWordDiff3() function
+
+    secretWordDiff4() {
+        while(this.newWord.length < 11) {
+            this.newWord = this.dictionary[Math.floor(Math.random() * this.dictionary.length)];
+        }
+    } // End of secretWordDiff4() function
+
     secretWord() {
-        let wordIndex = Math.floor(Math.random() * this.dictionary.length);
-        this.newWord = this.dictionary[wordIndex];
+        this.newWord = this.dictionary[Math.floor(Math.random() * this.dictionary.length)];
+        switch(this.difficulty) {
+            case 1:
+                this.secretWordDiff1();
+                break;
+
+            case 2:
+                this.secretWordDiff2();
+                break;
+
+            case 3:
+                this.secretWordDiff3();
+                break;
+            
+            case 4:
+                this.secretWordDiff4();
+                break;
+        }
         return this.newWord.length;
     } // End of secretWord() function
 
@@ -21,12 +61,12 @@ class ComputerPlayer {
 
     randomLetter() {
         return this.MOVES[Math.floor(Math.random() * 27) - 1];
-    }
+    } // End of randomLetter() function
 
     getMove(referee, guessesArr, board) {
         let newWordArr = referee.newWord.split("");
         let guess = this.randomLetter();
-
+        
         let guessNotRepeat = false;
         while(!guessNotRepeat) {
             if(!newWordArr.includes("a") && !newWordArr.includes("e") && !newWordArr.includes("i") && !newWordArr.includes("o") && !newWordArr.includes("u")) {
@@ -53,8 +93,6 @@ class ComputerPlayer {
 
 
     getMoveNoVowels(vowels, guess) {
-        // guess = this.randomLetter();
-
         while(vowels.includes(guess) || guess === undefined) {
             guess = this.randomLetter();
         } // End of loop if randomLetter() returns a vowel
@@ -62,9 +100,6 @@ class ComputerPlayer {
         return guess;
     } // End of getMoveNoVowels() function
 
-    // computerDifficulty() {
-    //     this.difficulty = Math.floor(Math.random() * 4) + 1;
-    // }
 }
 
 module.exports = ComputerPlayer;
