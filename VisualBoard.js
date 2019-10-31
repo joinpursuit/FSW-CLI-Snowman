c2844f0e8e96f3145b142b015e9a0f3eca304a55
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2019-10-31 09:49:28 -0400

    create VisualBoard.js and VisualBoard class

---
+++ VisualBoard.js
@@ -0,0 +1,96 @@
+class VisualBoard {
+    constructor() {
+
+    }
+
+    initialBoard() {
+        console.log("_________");
+        console.log("|       |");
+        console.log("        |");
+        console.log("        |");
+        console.log("        |");
+        console.log("     -------");
+    }
+
+    wrongGuess1Board() {
+        console.log("   _________");
+        console.log("   |       |");
+        console.log("  ( )      |");
+        console.log("           |");
+        console.log("           |");
+        console.log("        -------");
+    }
+
+    wrongGuess2Board() {
+        console.log("   _________");
+        console.log("   |       |");
+        console.log("  ( )      |");
+        console.log("   |       |");
+        console.log("           |");
+        console.log("        -------");
+    }
+
+    wrongGuess3Board() {
+        console.log("   _________");
+        console.log("   |       |");
+        console.log("  { }      |");
+        console.log("   |       |");
+        console.log("  /        |");
+        console.log("        -------");
+    }
+
+    wrongGuess4Board() {
+        console.log("   _________");
+        console.log("   |       |");
+        console.log("  { }      |");
+        console.log("   |       |");
+        console.log("  / \\      |");
+        console.log("        -------");
+
+    }
+
+    wrongGuess5Board() {
+        console.log("   _________");
+        console.log("   |       |");
+        console.log("  { }      |");
+        console.log("  /|       |");
+        console.log("  / \\      |");
+        console.log("        -------");
+    }
+
+    wrongGuess6Board() {
+        console.log("   _________");
+        console.log("   |       |");
+        console.log("  { }      |");
+        console.log("  /|\\      |");
+        console.log("  / \\      |");
+        console.log("        -------");
+    }
+
+    wrongGuess7Board() {
+        console.log("   _________");
+        console.log("   |       |");
+        console.log(" {;  }     |");
+        console.log("  /|\\      |");
+        console.log("  / \\      |");
+        console.log("        -------");
+    }
+
+    wrongGuess8Board() {
+        console.log("   _________");
+        console.log("   |       |");
+        console.log(" {; ;}     |");
+        console.log("  /|\\      |");
+        console.log("  / \\      |");
+        console.log("        -------");
+    }
+
+    wrongGuess9Board() {
+        console.log("   _________");
+        console.log("   |       |");
+        console.log(" {;.;}     |");
+        console.log("  /|\\      |");
+        console.log("  / \\      |");
+        console.log("        -------");
+    }
+}
