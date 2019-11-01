9874dc2df75d30aa5043bb84210137eb68b823d9
bsihame <sihamebazi@pursuit.org>
2019-11-01 17:00:45 -0400

    add guess validity

--- Game.js
+++ Game.js
@@ -18,18 +18,27 @@ class Game {
         //this.currentPlayer = player1; 
         this.board = new Board(this.computer.secretWordLength());
         this.guessesRemaining = 9;
-        this.guestLetter = [];
+        this.guessedLetters = [];
     }
 
    
-  isGameOver(){
+  isGameOver() {
     if(this.board.isComplete(this.computer.secretWordLength) || this.guessesRemaining === 0) {
       return true;
     } else {
       return false;
     }
 
+  }
+
+  guessValidity(letter) {
+    let alphabets = [“a”, “b”, “c”, “d”, “e”, “f”, “g”, “h”, “i”, “j”, “k”, “l”, “m”, “n”, “o”, “p”, “q”, “r”, “s”, “t”, “u”, “v”, “w”, “x”, “y”, “z”];
+    if (!this.guessedLetters.includes(letter) &&  alphabets.includes(letter)) {
+      return true;
+    } else {
+      return false;
     }
+  
   }
   
 
