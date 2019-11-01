35a770ba82f75caa7d65d6c225d2cb938eecfda8
bsihame <sihamebazi@pursuit.org>
2019-11-01 16:11:11 -0400

    add human player class

--- computerPlayer.js
+++ computerPlayer.js
@@ -1,7 +1,7 @@
 // ComputerPlayer 
 //   Should have a dictionary.
 //   Should have secretWord and return it's length. 
-//   * Should have a reveal. 
+//   Should have a reveal. 
 //   * Should give the positions of guessed characters. 
 //   * Possibly have a getMove. 
 const {dictionary} = require("./dictionary.js")
@@ -18,6 +18,8 @@ const {dictionary} = require("./dictionary.js")
   reveal() {
      return this.secretWord
   }
+
+  computerGuess
 }
 
   
--- humanPLayer.js
+++ humanPLayer.js
@@ -1,13 +1,19 @@
-  
+// HumanPlayer 
+// * Will need to getMove from player. 
+// * Display the board. 
+// * Possibly a select secret word that returns the length. 
+// * Possibly a givePostioins that returns an array of indicies. 
+// * Possibly a reveal word.  
+
 const readline = require("readline-sync");
 
 class HumanPlayer {
     constructor(name) {
-        this.name = name;
-       
+        this.name = name; 
     }
     getMove() {
-        return readline.question("Enter Your Move!")
+        let userGuess = readline.question("Please Enter A Letter!");
+        return userGuess.toLocaleLowerCase();
     }
 }
 
