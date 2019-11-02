47a7233fbcd5e439335dc0abb4b344c5a524f560
Marvin Thompson <marvinthompson@pursuit.org>
2019-11-01 23:35:20 -0400

    computer player

--- ComputerPlayer.js
+++ ComputerPlayer.js
@@ -1,22 +1,27 @@
 const {allTheWords} = require("./word.js")
+const Board = require("./Board.js")
 
 class ComputerPlayer {
     constructor() {
         this.dictionary = allTheWords
-        this.secretWord = ""        
+        this.word = ""        
     }
     
     secretWord() {
-        this.secretWord = Math.floor(Math.random() * this.dictionary.length)
-        return this.secretWord
+        for (let i = 0; i < this.dictionary.length; i++){
+        let words = Math.floor(Math.random() * this.dictionary.length)
+        return this.dictionary[words]
+        }
     }
 
     reveal() {
         return this.secretWord()
     }
 
-
 }
+
+let robo = new ComputerPlayer()
+console.log(robo.secretWord())
 
 module.exports = ComputerPlayer
 
