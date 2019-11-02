0a1bb413ce910157537b79bd9f93e0668bf48b37
Henrynunez112 <henrynunez@pursuit.org>
2019-11-01 21:25:56 -0400

    game 1

--- Game.js
+++ Game.js
@@ -1,5 +1,16 @@
 const HumanPlayer = require("./HumanPlayer.js")
 const ComputerPlayer = require("./ ComputerPlayer.js")
 const Board = require("./ Board.js")
+const readline = require("./readline - sync");
 class Game{
+    constructor(){
+        this.board = new Board()
+    }
+    validGuess(guess){
+        let alpha =
+        
+    }
+    isGameOver(){
+        if(this.guessed)
+    }
 }
