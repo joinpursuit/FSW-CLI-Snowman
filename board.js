27716fd7d5740b8d2e43f23224016ad5832cc2b7
dannyl <danilovlaurent@pursuit.org>
2019-11-01 21:29:33 -0400

    humanPlayer start

--- board.js
+++ board.js
@@ -30,7 +30,7 @@ class Board {
 }
 
 
-
+module.exports = Board;
 
 
 
--- humanPlayer.js
+++ humanPlayer.js
@@ -1,12 +1,26 @@
 const readline = require('readline-sync');
+const Board = require("./board.js")
+
 
 class HumanPlayer {
   constructor(name){
     this.name = name;
 
   }
-  getMove(){
-    return readline.question('Enter your Letter')
+  getMove(userInput){
+    userInput = readline.question('Enter your Letter:')
+    return userInput
+  }
+  displayBoard(){
+      let newBoard = new Board('word')
+      return newBoard
+
   }
 
 }
+let player1 = new HumanPlayer('danny')
+
+
+player1.displayBoard()
+
+
