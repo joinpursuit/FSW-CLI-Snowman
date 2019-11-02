83c0e16b6951a0a7311a12dcfc37b6db9f7dbb1b
Henrynunez112 <henrynunez@pursuit.org>
2019-11-01 23:18:38 -0400

    lost and not found... plzz help

--- Game.js
+++ Game.js
@@ -7,14 +7,14 @@ class Game{
         this.board = new Board(this.computer.secretWord());
         this.player = player;
         this.computer = new ComputerPlayer();
-        this.guessRemng = 
+        this.guessRemng = Infinity
         this.guessedWrd = []
     }
     validGuess(guess){
         let alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
-        let validWrd = false;
+            valid = false;
         if(alpha.includes(guess) && !this.guessedWrd.includes(guess)){
-            validWrd = true;
+            valid = true;
         }
         
     }
@@ -26,6 +26,25 @@ class Game{
         }
     }
     play(){
-        readline.question
+        console.log(`Hello! ${this.name} Welcome to my version of Hangman. Would you like play? press 'y' or 'no'`);
+        while(!this.isGameOver()){
+            this.board.displayDaBoard();
+            let guess = this.player.getMove();
+            while(this.guessedWrd.includes(guess)){
+
+            }
+        }if(this.validGuess(guess)){
+            this.guessedWrd.push(guess)
+            this.board.characDiffInd(this.computer.word, guess);
+            this.guessRemng -= 1;
+        }else{
+            guess = readline.question("")
+        }
+        if(this.guessRemng){
+            return this.computer.reveal()
+        }else{
+            console.log("You ran out of guesses! Sorry!")
+            console.log(this.computer.reveal())
+        }
     }
 }
