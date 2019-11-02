3a7902c00a4b69af3898b9051509d416fcaed0cc
Danielle R Cherry <daniellecherry@pursuit.org>
2019-11-02 13:28:31 -0400

    Board Complete Correct Order

--- Hangman/Board.js
+++ Hangman/Board.js
@@ -1,5 +1,5 @@
 const {words} = require("./words.js");
-const {ComputerPlayer} = require("./ComputerPlayer.js")
+// const {ComputerPlayer} = require("./ComputerPlayer.js")
 // const HumanPlayer = require("./HumanPlayer.js")
 
 class Board {
@@ -10,13 +10,17 @@ class Board {
         // this.ComputerPlayer = ComputerPlayer;
     }
     boardFill() {
-        const ref = new ComputerPlayer();
-        let str = ref.getSecretWord();
-        console.log(str)//DELETE!!
-        let chosenWord = str.split("");
-        for(let i = 0; i < chosenWord.length; i++) {
-            return chosenWord = chosenWord.fill("_");
-        }
+        // const ref = new ComputerPlayer();
+        // let str = ref.getSecretWord();
+        // console.log(str)//DELETE!!
+        // let chosenWord = str.split("");
+        for(let i in this.words)
+        this.board[i] = "_";
+        this.board = this.board.join("");
+        this.board.forEach((el,i)=> {
+             el[i] = this.board[i] = " _"
+        })
+        return el[i];
     }
     boardComplete() {
         if(this.board.join() === this.word) {
@@ -35,26 +39,26 @@ class Board {
 }
 
 
-// module.exports = {Board()}
+module.exports = {Board()}
 
-let boardA = new Board();
-console.log(boardA)
-// console.log(charsAdded())
-console.log(boardA.boardFill())
-// let danielle = new HumanPlayer();
-// console.log(boardA.boardFill)
-// console.log(danielle.getGreeting)
-// console.log(danielle.getMove)
-// console.log(boardA.boardFill());
-
-
+// let boardA = new Board();
+// console.log(boardA)
+// // console.log(charsAdded())
+// console.log(boardA.boardFill())
+// // let danielle = new HumanPlayer();
+// // console.log(boardA.boardFill)
+// // console.log(danielle.getGreeting)
+// // console.log(danielle.getMove)
+// // console.log(boardA.boardFill());
 
 
-// charsAdded(char){
-//     this.charsAdded = char
-//     for(let i = 0; i < this.words.length; i++) {
-//         if(this.words[i] === char) {
-//             this.board[i] = char;
-//         }
-//     }
-// }
+
+
+// // charsAdded(char){
+// //     this.charsAdded = char
+// //     for(let i = 0; i < this.words.length; i++) {
+// //         if(this.words[i] === char) {
+// //             this.board[i] = char;
+// //         }
+// //     }
+// // }
