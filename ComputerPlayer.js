a3824271e63a019b37f0222f2c78d173653596a1
Samantha Jimenez <samanthajimenez@pursuit.org>
2019-11-01 23:42:09 -0400

    complete computer player

--- ComputerPlayer.js
+++ ComputerPlayer.js
@@ -1,38 +1,31 @@
 const {allTheWords} = require('./words.js');
 
-let alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-
 class ComputerPlayer {
-    constructor() {
-        // this.word = allTheWords
+    constructor(computer) {
+        this.computer = computer;
         this.word = allTheWords[(Math.floor(Math.random() * allTheWords.length))];
     }
     returnLength() {
-        return this.word.length;
+        let secretWord = this.word;
+        return secretWord.length;
     }
     reveal() {
-        return this.word;
-    }
-    givePositions() {
-        let guessIdx = []
+        let revealStr = []; 
         for(let i = 0; i < this.word.length; i++) {
-            if(guessChar === this.word[i]) {
-                guessIdx.push(i)
+            revealStr.push(this.word[i]);
+        }
+        return revealStr.join(" ");
+    }
+    charGuess(letterGuess) {
+        let guessChar = []
+        for(let i = 0; i < this.word.length; i++) {
+            if(guessLetter === this.word[i]) {
+                guessChar.push(i);
             }
         }
-        return guessIdx
+        return guessChar;
     } 
-    // getMove() {
-    //    return alphabet[Math.random() * ]
-    // }
-    // secretWord() {
-    //     let secretWord = this.dictionary[Math.round(Math.random() * (this.dictionary.length) - 1)];
-    //     return secretWord.length;
-    // }
-
 }
 
-// let comp = new ComputerPlayer()
-// console.log(comp)
-// console.log(comp.returnLength());
-// console.log(comp.getMove());
+
+module.exports = ComputerPlayer
