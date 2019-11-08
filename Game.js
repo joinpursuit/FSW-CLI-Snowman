dbda81a11f754ccf322ad5e99229040a6aba900c
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2019-11-08 07:34:01 -0500

    add in numerous helper functions

--- Game.js
+++ Game.js
@@ -3,6 +3,7 @@ const HumanPlayer = require("./HumanPlayer.js");
 const ComputerPlayer = require("./ComputerPlayer.js");
 const readline = require('readline-sync');
 const Dictionary = require("./dictionary.js");
+const chalk = require("chalk")
 
 class Game {
     constructor(guesser, referee) {
@@ -18,43 +19,60 @@ class Game {
         }
     } // End of constructor
 
+    userInput(question) {
+        return readline.question(question);
+    } // End of userInput() function
+
+    isValidInput(userInput, ...args) {
+        return args.includes(userInput.toUpperCase());
+    } // End of isValidInput() function
+
+    isYesOrNo() {
+        let yn = this.userInput(chalk.yellow("(Y/N) "));
+
+        while(!this.isValidInput(yn, "Y", "N", "YES", "NO")) {
+            console.log(chalk.red("Please enter 'Y' or 'N'"));
+            yn = this.isYesOrNo();
+        }
+
+        return yn;
+    } // End of isYesOrNo() function;
+
     validNumberPlayers(playerInput) {
-        let validInput = false;
-        while(!validInput) {
-            switch(playerInput) {
-                case "2":
-                    this.player1 = new HumanPlayer();
-                    this.player2 = new HumanPlayer();
-                    this.setPlayerNames();
+        while(!this.isValidInput(playerInput, "2", "1")) {
+            console.log(chalk.red("Please input a valid number of players."));
+            this.userInput("How many players? (1 or 2) ");
+        }
 
-                    console.clear();
+        switch(playerInput) {
+            case "2":
+                this.player1 = new HumanPlayer();
+                this.player2 = new HumanPlayer();
+                this.setPlayerNames();
 
-                    this.setPlayerCharacter();
+                console.clear();
 
-                    console.clear();
+                this.setPlayerCharacter();
 
-                    validInput = true;
-                    break;
+                console.clear();
 
-                case "1":
-                    this.player1 = new HumanPlayer();
-                    this.player2 = new ComputerPlayer();
+                validInput = true;
+                break;
 
-                    this.setPlayerNames();
+            case "1":
+                this.player1 = new HumanPlayer();
+                this.player2 = new ComputerPlayer();
 
-                    console.clear();
+                this.setPlayerNames();
 
-                    this.setSoloCharacter();
+                console.clear();
 
-                    validInput = true;
-                    break;
+                this.setSoloCharacter();
 
-                default:
-                    console.log("Please input a valid number of players.");
-                    playerInput = readline.question("How many players? (1 or 2) ");
+                validInput = true;
+                break;
+                    
             } // End of playerInput switch
-
-        } // End of validInput loop
     } // End of validNumberPlayers() function
 
     isTwoPlayers() {
@@ -63,87 +81,86 @@ class Game {
 
     setPlayerNames() {
         if(this.player2 instanceof HumanPlayer) {
-            this.player1.name = readline.question("Player 1, input your name. ");
-            this.player2.name = readline.question("Player 2, input your name. ");
+            this.player1.name = this.userInput("Player 1, input your name. ");
+            this.player2.name = this.userInput("Player 2, input your name. ");
+
             while(this.player1.name === "" || this.player2.name === "") {
                 if(this.player1.name === "") {
-                    this.player1.name = readline.question("Player 1, input your name. ");
+                    this.player1.name = this.userInput("Player 1, input your name. ");
                 } else {
-                    this.player2.name = readline.question("Player 2, input your name. ");
+                    this.player2.name = this.userInput("Player 2, input your name. ");
                 }
-            }
+            } // End of undefined name check
+
         } else {
-            this.player1.name = readline.question("Input your name. ");
+            this.player1.name = this.userInput("Input your name. ");
+
             while(this.player1.name === "") {
-                this.player1.name = readline.question("Player 1, input your name. ");
-            }
+                this.player1.name = this.userInput("Player 1, input your name. ");
+            } // End of undefined name check
+
         }
     } // End of playerNames() function
 
     setPlayerCharacter() {
-        let p1Choice = readline.question(`${this.player1.name} would you like to be the [1] guesser or the [2] referee? `);
+        let p1Choice = this.userInput(`${this.player1.name} would you like to be the [1] guesser or the [2] referee? `);
 
-        let validInput = false;
-        while(!validInput) {
-            switch(p1Choice) {
-                case "1":
-                    this.guesser = this.player1;
-                    this.referee = this.player2;
+        while(!this.isValidInput(p1Choice, "1", "2")) {
+            console.log(chalk.red("Please enter either 1 or 2."));
+            p1Choice = this.userInput(`${this.player1.name} would you like to be the [1] guesser or the [2] referee? `);
+        }
 
-                    validInput = true;
-                    break;
+        switch(p1Choice) {
+            case "1":
+                this.guesser = this.player1;
+                this.referee = this.player2;
 
-                case "2": 
-                    this.guesser = this.player2;
-                    this.referee = this.player1;
+                validInput = true;
+                break;
 
-                    validInput = true;
-                    break;
+            case "2": 
+                this.guesser = this.player2;
+                this.referee = this.player1;
 
-                default: 
-                    console.log("Please enter either 1 or 2.");
-                    p1Choice = readline.question(`${this.player1.name} would you like to be the [1] guesser or the [2] referee? `);
+                validInput = true;
+                break;
             } // End of p1Choice switch
-        } // End of validInput loop
     }  // End of twoPlayerCharacterChoice() function
 
     setSoloCharacter() {
-        let playerChoice = readline.question("Would you like to be the [1] guesser or [2] referee? ");
+        let playerChoice = this.userInput("Would you like to be the [1] guesser or [2] referee? ");
+        console.clear();
 
-        let playerChoiceComplete = false;
-        while(!playerChoiceComplete) {
-            console.clear();
-            switch(playerChoice) {
-                case "1": 
-                    this.guesser = this.player1;
-                    this.referee = this.player2;
+        while(!this.isValidInput(playerChoice, "1", "2")) {
+            console.log("Please input '1' for guesser or '2' for referee")
+            playerChoice = this.userInput("Would you like to be the guesser or referee? ");
+        }
 
-                    this.difficultyChoice();
+        switch(playerChoice) {
+            case "1": 
+                this.guesser = this.player1;
+                this.referee = this.player2;
+
+                this.difficultyChoice();
                     
-                    playerChoiceComplete = true;
-                    break;
-                case "2":
-                    this.guesser = this.player2;
-                    this.referee = this.player1;
+                playerChoiceComplete = true;
+                break;
+            case "2":
+                this.guesser = this.player2;
+                this.referee = this.player1;
 
-                    playerChoiceComplete = true;
-                    break;
-                default: 
-                    console.log("Please input '1' for guesser or '2' for referee")
-                    playerChoice = readline.question("Would you like to be the guesser or referee? ");
+                playerChoiceComplete = true;
+                break;
             } // End of playerChoice switch
-        } // End of playerChoice check
-
-        return playerChoice;
-
-    } // End of guesserRefereeChoice() function
+            return playerChoice;
+    } // End setSoloCharacter() function
 
     difficultyChoice() {
-        let diffChoice = readline.question("Choose a difficulty for the computer. (1 - 4) ");
+        let diffChoice = this.userInput("Choose a difficulty for the computer. (1 - 4) ");
 
-        while(diffChoice !== "1" && diffChoice !== "2" && diffChoice !== "3" && diffChoice !== "4") {
-            console.log("Enter a number between 1 and 4.");
-            diffChoice = readline.question("Choose a difficulty for the computer. (1 - 4) ");
+        while(this.isValidInput(diffChoice, "1", "2", "3", "4")) {
+            console.log(chalk.red("Enter a number between 1 and 4."));
+            diffChoice = this.userInput("Choose a difficulty for the computer. (1 - 4) ");
         } // End of diffChoice validity check
 
         this.referee.setDifficulty(Number(diffChoice));
@@ -151,28 +168,25 @@ class Game {
         this.categoryChoice();
     } // End of difficultyChoice() function
 
-    categoryChoice() {
-        console.log(this.isRefereePlayer() ? `${this.referee.name} choose a category.` : `${this.guesser.name} choose a category.`);
-        console.log("Choose a category:");
+    displayCategories() {
         console.log("[1] Movies");
-        console.log("[2] TV shows")
+        console.log("[2] TV shows");
         console.log("[3] Miscellaneous");
         console.log("[4] Musicians");
         console.log("[5] Books");
         console.log("[6] Video Games");
-        let catChoice = readline.question("");
+    } // End of displayCategories() function
 
-        while(catChoice !== "1" && catChoice !== "2" && catChoice !== "3" && catChoice !== "4" && catChoice !== "5" && catChoice !== "6") {
+    categoryChoice() {
+        console.log(this.isRefereePlayer() ? `${this.referee.name} choose a category.` : `${this.guesser.name} choose a category.`);
+        this.displayCategories();
+        let catChoice = this.userInput("");
+
+        while(this.isValidInput(catChoice, "1", "2", "3", "4", "5", "6")) {
             console.clear();
-            console.log("Enter a number between 1 & 6")
-            console.log("Choose a category:");
-            console.log("[1] Movies");
-            console.log("[2] TV shows")
-            console.log("[3] Miscellaneous");
-            console.log("[4] Musicians");
-            console.log("[5] Books");
-            console.log("[6] Video Games");
-            catChoice = readline.question("");
+            console.log(chalk.red("Enter a number between 1 & 6"));
+            this.displayCategories();
+            catChoice = this.userInput("");
         }
 
         this.referee.setCategory(Number(catChoice));
@@ -191,29 +205,37 @@ class Game {
         return this.referee === this.player1 ? "player1" : "player2"
     } // End of findRefereePlayer() function
 
+    guesserWin() {
+        this.guesser.incrementWins();
+        if(this.isGuesserPlayer()) {
+            return `${this.guesser.name} has won! It took ${this.board.movesTaken} moves`;
+        } else {
+            return `Computer has won! It took ${this.board.movesTaken} moves`;
+        }
+    } // End of guesserWin() function
+
+    refereeWin() {
+        this.referee.incrementWins();
+        if(this.isGuesserPlayer()) {
+            if(this.isRefereePlayer()) {
+                return `${this.guesser.name} lost! ${this.referee.name} won!`;
+            } else {
+                return `${this.guesser.name} lost!`
+            }
+        } else {
+            return "You won!"
+        }
+    } // End of refereeWin() function
+
     playerWin() {
         console.clear();
-        this.board.buildVisualBoard();
+        this.referee.category === "misc" ? this.board.buildVisualBoardMisc() : this.board.buildVisualBoard();
         console.log(this.referee.revealWord(this.board));
 
         if(this.isGuesserWin()) {
-            this.guesser.incrementWins();
-            if(this.isGuesserPlayer()) {
-                return `${this.guesser.name} has won! It took ${this.board.movesTaken} moves`;
-            } else {
-                return `Computer has won! It took ${this.board.movesTaken} moves`;
-            }
+            this.guesserWin();
         } else {
-            this.referee.incrementWins();
-            if(this.isGuesserPlayer()) {
-                if(this.isRefereePlayer()) {
-                    return `${this.guesser.name} lost! ${this.referee.name} won!`;
-                } else {
-                    return `${this.guesser.name} lost!`
-                }
-            } else {
-                return "You won!"
-            }
+            this.refereeWin();
         }
     } // End of playerWin() function
 
@@ -240,7 +262,7 @@ class Game {
             console.log(computerGuess);
             this.board.movesTaken++;
 
-            readline.question("");
+            this.userInput("");
 
             this.board.isGameOver(this.board.board)
 
@@ -248,45 +270,43 @@ class Game {
 
     }// End of computerGuesser() function
 
+    guessWord() {
+        console.log(chalk.yellow("Would you like to guess the word?"));
+        let guessWord =  this.isYesOrNo();
+        if(guessWord.toUpperCase() === "Y") {
+            this.guessWordSure();
+        }
+
+    } // End of guessWord() function
+
+    guessWordSure() {
+        console.log(chalk.yellow("If you guess wrong you will lose. Are you sure?"));
+        let areYouSure = this.isYesOrNo();
+
+        if(areYouSure.toUpperCase() === "Y") {
+            console.log(this.board.guessWord(this.referee));
+        }
+
+        this.userInput(chalk.yellow("Press enter to continue."));
+    } // End of guessWordSure() function
+
     playerGuesser() {
         while(!this.board.isGameOver(this.board.board)) {
             console.clear();
 
             this.board.printBoard(this.referee);
 
-            console.log("Would you like to guess the word?");
-            let guessWord = readline.question("(Y/N) ");
-
-            while(guessWord.toUpperCase() !== "Y" && guessWord.toUpperCase() !== "N") {
-                console.log("Please enter 'Y' or 'N'");
-                guessWord = readline.question("");
+            this.guessWord();
+            if(this.board.isGameOver()) {
+                break;
             }
 
-            if(guessWord.toUpperCase() === "Y") {
-                console.log("If you guess wrong you will lose. Are you sure?");
-                let areYouSure = readline.question("(Y/N) ");
-
-                while(areYouSure.toUpperCase() !== "Y" && areYouSure.toUpperCase() !== "N") {
-                    console.log("Please enter 'Y' or 'N'");
-                    areYouSure = readline.question("");
-                }
-
-                if(areYouSure.toUpperCase() === "Y") {
-                    this.board.guessWord(this.referee);
-                }
-
-                let pressEnter = readline.question("Press enter to continue.");
-                if(this.board.isGameOver()) {
-                    break;
-                }
+            let guess = this.guesser.getMove();
+            while(!this.board.isValidMove(guess)) {
+                guess = this.guesser.getMove();
             }
 
-            this.board.placeLetter(this.guesser.getMove(), this.referee.newWord);
-
-            // while(!this.board.placeLetter(this.guesser.getMove(), this.referee.newWord)) {
-            //     console.log("Please enter a letter guess.");
-            //     this.board.placeLetter(this.guesser.getMove(), this.referee.newWord);
-            // }
+            this.board.placeLetter(guess, this.referee.newWord);
             
             this.board.movesTaken++;
 
@@ -298,32 +318,21 @@ class Game {
     
     isPlayAgain() {
         console.log("Would you like to play again? ");
-        let playAgain = readline.question("(Y/N) ");
-        while(playAgain.toUpperCase() !== "Y" && playAgain.toUpperCase() !== "N") {
-            console.log("Please enter 'Y' or 'N'");
-            playAgain = readline.question("");
-        }
-        switch(playAgain.toUpperCase()) {
-            case "Y":
-                return true;
-            
-            case "N":
-                return false;
-        }
+        let playAgain = this.isYesOrNo();
+
+        return playAgain.toUpperCase() === "Y";
     } // End of isPlayAgain() function
 
     displayLeaderboard() {
         let p1 = this.player1.name.toUpperCase();
         let p2 = this.player2.name ? this.player2.name : "CPU";
-        console.table({
-            [p1]: this.player1.wins,
-            [p2]: this.player2.wins
-        });
+
+        console.table([p1, p2], ["wins"]);
     } // End of displayLeaderboard() function
 
     play() {
         console.clear();
-        let playerNumber = readline.question("How many players? (1 or 2) ");
+        let playerNumber = this.userInput("How many players? (1 or 2) ");
         this.validNumberPlayers(playerNumber);
 
         let replay = false;
