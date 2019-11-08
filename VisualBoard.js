a5c4d0fa5ba2dfa0470531b461ba1159f08582bf
IsaiahCollazo99 <isaiahramoncollazo@gmail.com>
2019-11-08 07:34:39 -0500

    add chalk into the visual baord

--- VisualBoard.js
+++ VisualBoard.js
@@ -1,92 +1,94 @@
+const chalk = require('chalk');
+
 let visualBoard = [
     () => {
-        console.log("   _________");
-        console.log("   |       |");
-        console.log(" {;.;}     |");
-        console.log("  /|\\      |");
-        console.log("  / \\      |");
-        console.log("        -------");
+        console.log(chalk.magenta("   _________"));
+        console.log(chalk.magenta("   |       |"));
+        console.log(chalk.magenta(" {;.;}     |"));
+        console.log(chalk.magenta("  /|\\      |"));
+        console.log(chalk.magenta("  / \\      |"));
+        console.log(chalk.magenta("        -------"));
     }, // End of wrongGuess9Board() function
 
     () => {
-        console.log("   _________");
-        console.log("   |       |");
-        console.log(" {; ;}     |");
-        console.log("  /|\\      |");
-        console.log("  / \\      |");
-        console.log("        -------");
+        console.log(chalk.magenta("   _________"));
+        console.log(chalk.magenta("   |       |"));
+        console.log(chalk.magenta(" {; ;}     |"));
+        console.log(chalk.magenta("  /|\\      |"));
+        console.log(chalk.magenta("  / \\      |"));
+        console.log(chalk.magenta("        -------"));
     }, // End of wrongGuess8Board() function
 
     () => {
-        console.log("   _________");
-        console.log("   |       |");
-        console.log(" {;  }     |");
-        console.log("  /|\\      |");
-        console.log("  / \\      |");
-        console.log("        -------");
+        console.log(chalk.magenta("   _________"));
+        console.log(chalk.magenta("   |       |"));
+        console.log(chalk.magenta(" {;  }     |"));
+        console.log(chalk.magenta("  /|\\      |"));
+        console.log(chalk.magenta("  / \\      |"));
+        console.log(chalk.magenta("        -------"));
     }, // End of wrongGuess7Board() function
 
     () => {
-        console.log("   _________");
-        console.log("   |       |");
-        console.log(" {   }     |");
-        console.log("  /|\\      |");
-        console.log("  / \\      |");
-        console.log("        -------");
+        console.log(chalk.magenta("   _________"));
+        console.log(chalk.magenta("   |       |"));
+        console.log(chalk.magenta(" {   }     |"));
+        console.log(chalk.magenta("  /|\\      |"));
+        console.log(chalk.magenta("  / \\      |"));
+        console.log(chalk.magenta("        -------"));
     }, // End of wrongGuess6Board() function
 
     () => {
-        console.log("   _________");
-        console.log("   |       |");
-        console.log(" {   }     |");
-        console.log("  /|       |");
-        console.log("  / \\      |");
-        console.log("        -------");
+        console.log(chalk.magenta("   _________"));
+        console.log(chalk.magenta("   |       |"));
+        console.log(chalk.magenta(" {   }     |"));
+        console.log(chalk.magenta("  /|       |"));
+        console.log(chalk.magenta("  / \\      |"));
+        console.log(chalk.magenta("        -------"));
     }, // End of wrongGuess5Board() function
 
     () => {
-            console.log("   _________");
-            console.log("   |       |");
-            console.log(" {   }     |");
-            console.log("   |       |");
-            console.log("  / \\      |");
-            console.log("        -------");
+            console.log(chalk.magenta("   _________"));
+            console.log(chalk.magenta("   |       |"));
+            console.log(chalk.magenta(" {   }     |"));
+            console.log(chalk.magenta("   |       |"));
+            console.log(chalk.magenta("  / \\      |"));
+            console.log(chalk.magenta("        -------"));
     }, // End of wrongGuess4Board() function
 
     () => {
-        console.log("   _________");
-        console.log("   |       |");
-        console.log(" {   }     |");
-        console.log("   |       |");
-        console.log("  /        |");
-        console.log("        -------");
+        console.log(chalk.magenta("   _________"));
+        console.log(chalk.magenta("   |       |"));
+        console.log(chalk.magenta(" {   }     |"));
+        console.log(chalk.magenta("   |       |"));
+        console.log(chalk.magenta("  /        |"));
+        console.log(chalk.magenta("        -------"));
     }, // End of wrongGuess3Board() function
 
     () => {
-        console.log("   _________");
-        console.log("   |       |");
-        console.log(" {   }     |");
-        console.log("   |       |");
-        console.log("           |");
-        console.log("        -------");
+        console.log(chalk.magenta("   _________"));
+        console.log(chalk.magenta("   |       |"));
+        console.log(chalk.magenta(" {   }     |"));
+        console.log(chalk.magenta("   |       |"));
+        console.log(chalk.magenta("           |"));
+        console.log(chalk.magenta("        -------"));
     }, // End of wrongGuess2Board() function
 
     () => {
-        console.log("   _________");
-        console.log("   |       |");
-        console.log(" {   }     |");
-        console.log("           |");
-        console.log("           |");
-        console.log("        -------");
+        console.log(chalk.magenta("   _________"));
+        console.log(chalk.magenta("   |       |"));
+        console.log(chalk.magenta(" {   }     |"));
+        console.log(chalk.magenta("           |"));
+        console.log(chalk.magenta("           |"));
+        console.log(chalk.magenta("        -------"));
     }, // End of wrongGuess1Board() function
 
     () => {
-        console.log("   _________");
-        console.log("   |       |");
-        console.log("           |");
-        console.log("           |");
-        console.log("           |");
-        console.log("        -------");
+        console.log(chalk.magenta("   _________"));
+        console.log(chalk.magenta("   |       |"));
+        console.log(chalk.magenta("           |"));
+        console.log(chalk.magenta("           |"));
+        console.log(chalk.magenta("           |"));
+        console.log(chalk.magenta("        -------"));
     } // End of initialBoard() function
 ]
 
