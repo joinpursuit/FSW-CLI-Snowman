ca35d040dbec2880f9ab206112187530a0030f8c
DejaLFlynn <dejaflynn@pursuit.org>
2020-01-02 22:22:19 -0500

    newHangman

--- game.js
+++ game.js
@@ -1,85 +1,31 @@
-const player = require('./player.js')
-const computerPlayer = require('./computerPlayer.js')
-const board = require('./board.js')
-const player = require('./player.js')
+const readlineSync = require("readline-sync")
+const Board = require('./Board.js')
 
-class Game {
-  constructor(players) {
-    this.ref = players['ref']
-    this.guesser = players['guesser']
-    this.guessesRemaining = 6
-    this.guessLetters = []
-    
+class Game{
+    constructor(words){
+       this.words = words 
+       this.chances = 6
 
-  }
-  play() {
-    let boardLength = this.ref.chooseSecretWord()
-    this.board = new board(boardLength)
-    while (!this.isGameOver()) {
-      console.log('letters guessed so far')
-      console.log('guessed dso far'+ this.guessesRemaining.join(","))
-      this.guesser.displayBoard(this.board)
-      let guess ='0';
- 
-      while (this.isValidGuess(guess)){
-        guess = this.guesser.getMove()
-        if (!this.isValidGuess(guess)){
-          console.log('invalid gues')
-        }
-      }
-      let positions = this.ref.checkGuess(guess)
-      if (positions.length === 0){
-        this.board.addChar(positions, guess)
-        this.guessesRemaining --
-      }
-     if (this.guessesRemaining> 0){
-       console.log(this.guesser.name + 'wins')
-     }else{
-       console.log(this.ref.name+'wins')
-     }
     }
-  }
+    playGame(){
+        let possibleWord = getWord(words)
+        let hidden = pickWord(possibleWord)
+        console.log(hidden.join(""))
+        let chances = 6
+        while(chances>0 && hidden.join("" !==possibleWord)){
+            const currWord = readline.question("type a letter please")
+            if(currLetter(currWord,possibleWord)){
+                console.log("that is correct")
+                possibleWord = nextLetter(currWord,possibleWord,hidden)
+                console.log(hidden)
+            }else{
+                console.log("that is incorrect")
+                console.log(`you have ${chances -1}left`)
+                chances --
+            }
 
-  getValidLetterGuess(char) {
-    let letters = 'abcdefghijklmnopqrstuvwxyz'
-    return char.length === 1 && letters.includes(char) && this.guessLetters.has(has)
-
-  }
-  isGameOver() {
-    return this.board.isComplete() || this.guessesRemaining === 0
-  }
-
-
-// function getValidLetterGuess() {
-//   function guessIsValid(letter) {
-//     return letter.length === 1 && letter.toUpperCase() != letter.toLowerCase()
-//   }
-//   let letter = ""
-//   while (!letter) {
-//     let input = readline.question("Please enter your guess: ")
-//     if (guessIsValid(input)) {
-//       letter = input
-//     } else {
-//       console.log("Please enter a valid letter")
-//     }
-//   }
-//   return letter.toLowerCase()
-// }
-
-
-
-
-
-// what is needed for hangman
-
-
-// an Array of words
-
-// check indices for each letter of word
-// delete chosen used letters 
-// print  correct letter to replace blanks
-
-module.exports = game
-let human =new HumanPlayer('deja')
-let robo = new computerPlayer()
-let game = new Game([ref:])
+        }
+    }
+    
+}
+module.exports = Game;
--- player.js
+++ player.js
@@ -1,38 +1,73 @@
 const readline = require('readline-sync')
-class HumanPlayer {
-    constructor(name) {
-        this.name = name
-    }
-    length(){
-        return this.board.length
-    }
-    get (idx){
-        return this.board[idx]
-    }
-    getMove(){
-        return readline.question( 'would make a move here').toLowerCase()
-       
-    }
-     displayBoard(board){
-         let output = ''
-        for(let i = 0; i < board.length(); i++){
-            output +=board.get(i)+ " "
-        }
-        console.log(output)
-    }
+const Game = require("./Game.js")
+const Board = require("./Board")
+
+class Player{
+  constructor(name = 'Robo'){
+    this.wordBank = words
+    this.name = name
+  }
+  chooseSecretWord(){
+    let idx = Math.floor(Math.random()*this.wordBank.length)
+    this.hiddenWord = this.wordBank[idx]
+    return this.hiddenWord.length
     
+  }
+  checkGuess(char){
+    let indices = []
+    for(let i = 0; i < this.hiddenWord.length; i++){
+      if(this.hiddenWord[i] === char){
+       indices.push(i)
+      }
+    }
+    return indices
+     }
+     display(board){
+       this.board = board;
+     }
+     turn(){
+        let charBank ="qwertyuiopasdfghjklzxcvbnm"
+        this.wordBank = this.wordBank.filter(el=>el.length ===this.board.length())
+        let incorrect = new Set([...this.guessMade])
+        for(let i = 0; i< this.board.length(); i++){
+          let char = this.board.get(i)
+          if(char !== "_"){
+            incorrect.delete(char)
+            this.wordBank = this.wordBank.filter(word=>word[i]===char);
+          }
+        }
+        incorrect.forEach(char=>{
+    
+        })
+        let charCount = {}
+        this.wordBank.forEach(word=>{
+          for(let char of word){
+            charCount[char] ? charCount[char]++ : charCount[char]=1;
+          }
+        })
+        let maximum = 0;
+        let guess = null
+        for(let char in charCount){
+          if(charCount[char]> maximum && !this.guessMade.has(char)){
+            maximum = charCount[char]
+            guess = char
+          }
+        }
+        while(!guess){
+          let random = Math.floor(Math.random()*charBank.length)
+          let letter = charBank[random]
+          if(!this.guessMade.has(letter)){
+            guess = letter
+          }
+        }
+        this.guessMade.add(guess)
+        return guess
+      }
+      display(board){
+        this.board = board;
+      }
+      reveal(){
+        return this.hidden
+      }
 }
-
-
-
-
-
-
-
-
-module.exports = player;
-let board = require('./board.js')
-let test = new Board(6)
-let deja = new HumanPlayer('deja')
-console.log(deja.getMove())
-deja.displayBoard(test)
+module.exports = Player;
