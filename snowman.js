95df1a991f169d6b1ac09aea6b7f8dee2c73b214
Flower Aviles <itsfloweraviles@gmail.com>
2021-01-16 02:45:13 -0500

    finally previous bugs however found myself in a loop

--- snowman.js
+++ snowman.js
@@ -1,10 +1,11 @@
 const readline = require('readline-sync');
 const dictionary = ["able", "about", "account", "acid", "across", "addition", "adjustment", "advertisement", "after", "again", "against", "agreement", "almost", "among", "amount", "amusement", "angle", "angry", "animal", "answer", "apparatus", "apple", "approval", "arch", "argument", "army", "attack", "attempt", "attention", "attraction", "authority", "automatic", "awake", "baby", "back", "balance", "ball", "band", "base", "basin", "basket", "bath", "beautiful", "because", "before", "behaviour", "belief", "bell", "bent", "berry", "between", "bird", "birth", "bite", "bitter", "black", "blade", "blood", "blow", "blue", "board", "boat", "body", "boiling", "bone", "book", "boot", "bottle", "brain", "brake", "branch", "brass", "bread", "breath", "brick", "bridge", "bright", "broken", "brother", "brown", "brush", "bucket", "building", "bulb", "burn", "burst", "business", "butter", "button", "cake", "camera", "canvas", "card", "care", "carriage", "cart", "cause", "certain", "chain", "chalk", "chance", "change", "cheap", "cheese", "chemical", "chest", "chief", "chin", "church", "circle", "clean", "clear", "clock", "cloth", "cloud", "coal", "coat", "cold", "collar", "colour", "comb", "come", "comfort", "committee", "common", "company", "comparison", "competition", "complete", "complex", "condition", "connection", "conscious", "control", "cook", "copper", "copy", "cord", "cork", "cotton", "cough", "country", "cover", "crack", "credit", "crime", "cruel", "crush", "current", "curtain", "curve", "cushion", "damage", "danger", "dark", "daughter", "dead", "dear", "death", "debt", "decision", "deep", "degree", "delicate", "dependent", "design", "desire", "destruction", "detail", "development", "different", "digestion", "direction", "dirty", "discovery", "discussion", "disease", "disgust", "distance", "distribution", "division", "door", "doubt", "down", "drain", "drawer", "dress", "drink", "driving", "drop", "dust", "early", "earth", "east", "edge", "education", "effect", "elastic", "electric", "engine", "enough", "equal", "error", "even", "event", "ever", "every", "example", "exchange", "existence", "expansion", "experience", "expert", "face", "fact", "fall", "false", "family", "farm", "father", "fear", "feather", "feeble", "feeling", "female", "fertile", "fiction", "field", "fight", "finger", "fire", "first", "fish", "fixed", "flag", "flame", "flat", "flight", "floor", "flower", "fold", "food", "foolish", "foot", "force", "fork", "form", "forward", "fowl", "frame", "free", "frequent", "friend", "from", "front", "fruit", "full", "future", "garden", "general", "girl", "give", "glass", "glove", "goat", "gold", "good", "government", "grain", "grass", "great", "green", "grey", "grip", "group", "growth", "guide", "hair", "hammer", "hand", "hanging", "happy", "harbour", "hard", "harmony", "hate", "have", "head", "healthy", "hear", "hearing", "heart", "heat", "help", "high", "history", "hole", "hollow", "hook", "hope", "horn", "horse", "hospital", "hour", "house", "humour", "idea", "important", "impulse", "increase", "industry", "insect", "instrument", "insurance", "interest", "invention", "iron", "island", "jelly", "jewel", "join", "journey", "judge", "jump", "keep", "kettle", "kick", "kind", "kiss", "knee", "knife", "knot", "knowledge", "land", "language", "last", "late", "laugh", "lead", "leaf", "learning", "leather", "left", "letter", "level", "library", "lift", "light", "like", "limit", "line", "linen", "liquid", "list", "little", "living", "lock", "long", "look", "loose", "loss", "loud", "love", "machine", "make", "male", "manager", "mark", "market", "married", "mass", "match", "material", "meal", "measure", "meat", "medical", "meeting", "memory", "metal", "middle", "military", "milk", "mind", "mine", "minute", "mist", "mixed", "money", "monkey", "month", "moon", "morning", "mother", "motion", "mountain", "mouth", "move", "much", "muscle", "music", "nail", "name", "narrow", "nation", "natural", "near", "necessary", "neck", "need", "needle", "nerve", "news", "night", "noise", "normal", "north", "nose", "note", "number", "observation", "offer", "office", "only", "open", "operation", "opinion", "opposite", "orange", "order", "organization", "ornament", "other", "oven", "over", "owner", "page", "pain", "paint", "paper", "parallel", "parcel", "part", "past", "paste", "payment", "peace", "pencil", "person", "physical", "picture", "pipe", "place", "plane", "plant", "plate", "play", "please", "pleasure", "plough", "pocket", "point", "poison", "polish", "political", "poor", "porter", "position", "possible", "potato", "powder", "power", "present", "price", "print", "prison", "private", "probable", "process", "produce", "profit", "property", "prose", "protest", "public", "pull", "pump", "punishment", "purpose", "push", "quality", "question", "quick", "quiet", "quite", "rail", "rain", "range", "rate", "reaction", "reading", "ready", "reason", "receipt", "record", "regret", "regular", "relation", "religion", "representative", "request", "respect", "responsible", "rest", "reward", "rhythm", "rice", "right", "ring", "river", "road", "roll", "roof", "room", "root", "rough", "round", "rule", "safe", "sail", "salt", "same", "sand", "scale", "school", "science", "scissors", "screw", "seat", "second", "secret", "secretary", "seed", "seem", "selection", "self", "send", "sense", "separate", "serious", "servant", "shade", "shake", "shame", "sharp", "sheep", "shelf", "ship", "shirt", "shock", "shoe", "short", "shut", "side", "sign", "silk", "silver", "simple", "sister", "size", "skin", "skirt", "sleep", "slip", "slope", "slow", "small", "smash", "smell", "smile", "smoke", "smooth", "snake", "sneeze", "snow", "soap", "society", "sock", "soft", "solid", "some", "song", "sort", "sound", "soup", "south", "space", "spade", "special", "sponge", "spoon", "spring", "square", "stage", "stamp", "star", "start", "statement", "station", "steam", "steel", "stem", "step", "stick", "sticky", "stiff", "still", "stitch", "stocking", "stomach", "stone", "stop", "store", "story", "straight", "strange", "street", "stretch", "strong", "structure", "substance", "such", "sudden", "sugar", "suggestion", "summer", "support", "surprise", "sweet", "swim", "system", "table", "tail", "take", "talk", "tall", "taste", "teaching", "tendency", "test", "than", "that", "then", "theory", "there", "thick", "thin", "thing", "this", "thought", "thread", "throat", "through", "through", "thumb", "thunder", "ticket", "tight", "till", "time", "tired", "together", "tomorrow", "tongue", "tooth", "touch", "town", "trade", "train", "transport", "tray", "tree", "trick", "trouble", "trousers", "true", "turn", "twist", "umbrella", "under", "unit", "value", "verse", "very", "vessel", "view", "violent", "voice", "waiting", "walk", "wall", "warm", "wash", "waste", "watch", "water", "wave", "weather", "week", "weight", "well", "west", "wheel", "when", "where", "while", "whip", "whistle", "white", "wide", "will", "wind", "window", "wine", "wing", "winter", "wire", "wise", "with", "woman", "wood", "wool", "word", "work", "worm", "wound", "writing", "wrong", "year", "yellow", "yesterday", "young"]
 
-
+let secretWord = dictionary[Math.floor(Math.random() * dictionary.length)]
 let guessCount = {maxGuess: 8, usedLetter: []}
-let secretAnswer = []
-let secretWordsArr = []
+let dash = []
+let eachDash = []
+
 
 //Welcome function
 const welcomeToStartGame = () => { // Added a welcome function! 
@@ -21,13 +22,12 @@ const welcomeToStartGame = () => { // Added a welcome function!
 //secretVocab 
 const secretVocab = () => {
   console.log("Great! Let's Go!!!")
-  let secretWord = dictionary[Math.floor(Math.random() * dictionary.length)]
-  let dash = "" // assigning a varible to dash and giving it an actual "_"
-  for (let i = 0; i < secretWord.length; i++){ // running a loop through every letter in the word 
-    dash += "_" + " " // would print out dashes instead of the word 
-   
-  } 
-  console.log(dash) // I am console logging here because it will print the words in dashes 
+  dash = secretWord.split("") //Assigning an empty array to each index 
+  dash.forEach((abc) => { //Making a new function to make a dash at each letter. 
+    eachDash.push("_") // pushing into every word to make a dash and giving it an actual "_"
+  }) 
+  secretAnswer = eachDash.join(" ")
+  console.log(secretAnswer)
   guessTheSecret(); // I am calling to anoter function 
 //console.log("Here's your word" + " " +secretWord) // This just logs the word. Need to learn how to get the word to print in secret! 
 }
@@ -50,45 +50,46 @@ const guessTheSecret = () => {
 
 //rightOrWrongGuess
 const rightOrWrongGuess = (guess) => { //With the "guess" param i am able to fun this function through the getValidLetter function. 
-  if(secretWordsArr.includes(guess)) {
-    guessRight(secretWordsArr, guess)
+  if(dash.includes(guess)) { // Setting the conditions that every dash will includee the guess param. 
+    guessRight(dash, guess)
   } else {
     guessWrong()
   }
 }
 
 // //guessRight
-const guessRight= (secretWordsArr, guess) => {
-  let letterOfWord = []
-  secretWordsArr.forEach((letter, i) => {
-    if (letter === guess){
-      letterOfWord.push(i)
+const guessRight = (dash, guess) => {
+  let letters = []
+  dash.forEach((abc, i) => {
+    if (abc === guess){
+      letters.push(i)
     }
   })
-  letterOfWord.forEach((letter) => {
-    secretAnswer.splice(letter, 1, guess)
+  letters.forEach((abc) => {
+    eachDash.splice(abc, 1, guess)
   }) 
-  answer = secretAnswer.join(" ")
+  secretAnswer = eachDash.join(" ")
   guessCount.usedLetter.push(guess)
   usedLettersTogether = guessCount.usedLetter.join(" ") 
-  console.log(`What you've guessed so far: ${usedLettersTogether}`)
-  console.log(answer)
-  if (dash < 0) { 
+  console.log(`Letters you have used : ${usedLettersTogether}`)
+  console.log(secretAnswer)
+  if (secretAnswer.indexOf("_") < 0) { 
     console.log('Hey!!! You Won!!!')
   } else {
     guessTheSecret();
   }
 }
 //guessWrong
-const guessWrong = (guess) => {
+const guessWrong = () => {
   guessCount.usedLetter.push(guess)
-  allTheLetters = guessCount.usedLetter.join (" ")
-  console.log(`Letters you have used : ${allTheLetters}`)
+  usedLettersTogether = guessCount.usedLetter.join (" ")
+  console.log(`Letters you have used : ${usedLettersTogether}`)
   guessCount.maxGuess --
   console.log("Yikes! Try again!")
-  console.log(answer)
+  console.log(secretAnswer)
   if (guessCount.maxGuess === 0) {
     console.log(`Dang! You have no chances left! You lose!`)
+    console.log(`The secret word was : ${secretWord}`)
     console.log(`Better luck next time ${yourName}`) 
     if (readline.keyInYN(`Would you like another chance to prove that this machine is infirior to you ${yourName}`)){
       secretVocab();
@@ -110,7 +111,7 @@ function getValidLetterGuess() {
     let input = readline.question("Please enter your guess: ")
     if (guessIsValid(input)) {
       letter = input
-      guessRight();
+      guessRight(dash);
     } else {
       console.log("Please enter a valid letter")
     }
@@ -125,3 +126,4 @@ const leaveGame = () => {
 }
 
 welcomeToStartGame();
+guessWrong ();
