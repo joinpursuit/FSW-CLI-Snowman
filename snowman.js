be964393e49b5e55dab2853aed7ddc6c9f040984
Dequisa <Dequisas@gmail.com>
2021-01-16 23:26:35 -0500

    Second Draft

--- snowman.js
+++ snowman.js
@@ -2,10 +2,54 @@ const readline = require('readline-sync');
 const dictionary = ["able", "about", "account", "acid", "across", "addition", "adjustment", "advertisement", "after", "again", "against", "agreement", "almost", "among", "amount", "amusement", "angle", "angry", "animal", "answer", "apparatus", "apple", "approval", "arch", "argument", "army", "attack", "attempt", "attention", "attraction", "authority", "automatic", "awake", "baby", "back", "balance", "ball", "band", "base", "basin", "basket", "bath", "beautiful", "because", "before", "behaviour", "belief", "bell", "bent", "berry", "between", "bird", "birth", "bite", "bitter", "black", "blade", "blood", "blow", "blue", "board", "boat", "body", "boiling", "bone", "book", "boot", "bottle", "brain", "brake", "branch", "brass", "bread", "breath", "brick", "bridge", "bright", "broken", "brother", "brown", "brush", "bucket", "building", "bulb", "burn", "burst", "business", "butter", "button", "cake", "camera", "canvas", "card", "care", "carriage", "cart", "cause", "certain", "chain", "chalk", "chance", "change", "cheap", "cheese", "chemical", "chest", "chief", "chin", "church", "circle", "clean", "clear", "clock", "cloth", "cloud", "coal", "coat", "cold", "collar", "colour", "comb", "come", "comfort", "committee", "common", "company", "comparison", "competition", "complete", "complex", "condition", "connection", "conscious", "control", "cook", "copper", "copy", "cord", "cork", "cotton", "cough", "country", "cover", "crack", "credit", "crime", "cruel", "crush", "current", "curtain", "curve", "cushion", "damage", "danger", "dark", "daughter", "dead", "dear", "death", "debt", "decision", "deep", "degree", "delicate", "dependent", "design", "desire", "destruction", "detail", "development", "different", "digestion", "direction", "dirty", "discovery", "discussion", "disease", "disgust", "distance", "distribution", "division", "door", "doubt", "down", "drain", "drawer", "dress", "drink", "driving", "drop", "dust", "early", "earth", "east", "edge", "education", "effect", "elastic", "electric", "engine", "enough", "equal", "error", "even", "event", "ever", "every", "example", "exchange", "existence", "expansion", "experience", "expert", "face", "fact", "fall", "false", "family", "farm", "father", "fear", "feather", "feeble", "feeling", "female", "fertile", "fiction", "field", "fight", "finger", "fire", "first", "fish", "fixed", "flag", "flame", "flat", "flight", "floor", "flower", "fold", "food", "foolish", "foot", "force", "fork", "form", "forward", "fowl", "frame", "free", "frequent", "friend", "from", "front", "fruit", "full", "future", "garden", "general", "girl", "give", "glass", "glove", "goat", "gold", "good", "government", "grain", "grass", "great", "green", "grey", "grip", "group", "growth", "guide", "hair", "hammer", "hand", "hanging", "happy", "harbour", "hard", "harmony", "hate", "have", "head", "healthy", "hear", "hearing", "heart", "heat", "help", "high", "history", "hole", "hollow", "hook", "hope", "horn", "horse", "hospital", "hour", "house", "humour", "idea", "important", "impulse", "increase", "industry", "insect", "instrument", "insurance", "interest", "invention", "iron", "island", "jelly", "jewel", "join", "journey", "judge", "jump", "keep", "kettle", "kick", "kind", "kiss", "knee", "knife", "knot", "knowledge", "land", "language", "last", "late", "laugh", "lead", "leaf", "learning", "leather", "left", "letter", "level", "library", "lift", "light", "like", "limit", "line", "linen", "liquid", "list", "little", "living", "lock", "long", "look", "loose", "loss", "loud", "love", "machine", "make", "male", "manager", "mark", "market", "married", "mass", "match", "material", "meal", "measure", "meat", "medical", "meeting", "memory", "metal", "middle", "military", "milk", "mind", "mine", "minute", "mist", "mixed", "money", "monkey", "month", "moon", "morning", "mother", "motion", "mountain", "mouth", "move", "much", "muscle", "music", "nail", "name", "narrow", "nation", "natural", "near", "necessary", "neck", "need", "needle", "nerve", "news", "night", "noise", "normal", "north", "nose", "note", "number", "observation", "offer", "office", "only", "open", "operation", "opinion", "opposite", "orange", "order", "organization", "ornament", "other", "oven", "over", "owner", "page", "pain", "paint", "paper", "parallel", "parcel", "part", "past", "paste", "payment", "peace", "pencil", "person", "physical", "picture", "pipe", "place", "plane", "plant", "plate", "play", "please", "pleasure", "plough", "pocket", "point", "poison", "polish", "political", "poor", "porter", "position", "possible", "potato", "powder", "power", "present", "price", "print", "prison", "private", "probable", "process", "produce", "profit", "property", "prose", "protest", "public", "pull", "pump", "punishment", "purpose", "push", "quality", "question", "quick", "quiet", "quite", "rail", "rain", "range", "rate", "reaction", "reading", "ready", "reason", "receipt", "record", "regret", "regular", "relation", "religion", "representative", "request", "respect", "responsible", "rest", "reward", "rhythm", "rice", "right", "ring", "river", "road", "roll", "roof", "room", "root", "rough", "round", "rule", "safe", "sail", "salt", "same", "sand", "scale", "school", "science", "scissors", "screw", "seat", "second", "secret", "secretary", "seed", "seem", "selection", "self", "send", "sense", "separate", "serious", "servant", "shade", "shake", "shame", "sharp", "sheep", "shelf", "ship", "shirt", "shock", "shoe", "short", "shut", "side", "sign", "silk", "silver", "simple", "sister", "size", "skin", "skirt", "sleep", "slip", "slope", "slow", "small", "smash", "smell", "smile", "smoke", "smooth", "snake", "sneeze", "snow", "soap", "society", "sock", "soft", "solid", "some", "song", "sort", "sound", "soup", "south", "space", "spade", "special", "sponge", "spoon", "spring", "square", "stage", "stamp", "star", "start", "statement", "station", "steam", "steel", "stem", "step", "stick", "sticky", "stiff", "still", "stitch", "stocking", "stomach", "stone", "stop", "store", "story", "straight", "strange", "street", "stretch", "strong", "structure", "substance", "such", "sudden", "sugar", "suggestion", "summer", "support", "surprise", "sweet", "swim", "system", "table", "tail", "take", "talk", "tall", "taste", "teaching", "tendency", "test", "than", "that", "then", "theory", "there", "thick", "thin", "thing", "this", "thought", "thread", "throat", "through", "through", "thumb", "thunder", "ticket", "tight", "till", "time", "tired", "together", "tomorrow", "tongue", "tooth", "touch", "town", "trade", "train", "transport", "tray", "tree", "trick", "trouble", "trousers", "true", "turn", "twist", "umbrella", "under", "unit", "value", "verse", "very", "vessel", "view", "violent", "voice", "waiting", "walk", "wall", "warm", "wash", "waste", "watch", "water", "wave", "weather", "week", "weight", "well", "west", "wheel", "when", "where", "while", "whip", "whistle", "white", "wide", "will", "wind", "window", "wine", "wing", "winter", "wire", "wise", "with", "woman", "wood", "wool", "word", "work", "worm", "wound", "writing", "wrong", "year", "yellow", "yesterday", "young"]
 
 
+let quitGame = ()=>{
+  process.exit()
+}
+function intro  () {
+  console.log('Welcome to Snowman')
+  if(readline.keyInYN('Would you like to play?')){ 
+  }else{
+    quitGame()
+  }
+}
+intro()
+
 let totNumOfGuess = 0
+let numOfGuess = 10
+let listOfGuess = {} 
+
+
+
+
+
+let randomWord = ()=> {
+  let string = ''
+  let randomNum =Math.floor(Math.random()* dictionary.length)
+    string = dictionary[randomNum];
+    return string
+  }
+  let hiddenWord = randomWord()
+
+  let blank = 'A'
+  function showBlanks () {
+    blank =''
+    for(let i = 0; i< hiddenWord.length;i++){
+        blank = `${blank} _`
+  
+    }
+    return blank
+  }
+  
+showBlanks()
+console.log(blank)
+
+
+
+  while(true){
 
 // promts user for word
-function getValidLetterGuess() {
+function getValidLetterGuess (){
+  console.log(`Your current guess count is ${numOfGuess}.`)
   function guessIsValid(letter) {
     totNumOfGuess +=1
     return letter.length === 1 && letter.toUpperCase() != letter.toLowerCase()
@@ -25,33 +69,37 @@ let guess = getValidLetterGuess()
 
 
 
-
-let randomWord = ()=> {
-  let string = ''
-  let randomNum =Math.floor(Math.random()* dictionary.length)
-    string = dictionary[randomNum];
-    return string
+function lowerByOne  () {
+  let array = hiddenWord.split('')
+  let wrong = (element) => element !== guess;
+  let contain = array.every(wrong)
+  if(contain=== true){
+  numOfGuess -= 1
   }
- 
+if(numOfGuess >=1){
+console.log(`you have ${numOfGuess} guesses remaining.`)
+} else{
+  console.log(`You have ${numOfGuess} guess remaining.`)
+}
+}
 
-let hiddenWord = randomWord()
+
+
+function winLose () {
+if(hiddenWord === blank){
+  console.log(`You've defeated the snowman. Congratulations!! \n ${totNumOfGuess}`)
+} else if(numOfGuess<=0){
+  console.log(`You've been defeated by the snowman. \n The word was ${hiddenWord}`)
+  quitGame()
+}
+}
+winLose()
+
+
 console.log(hiddenWord)
 
-let blank = 'A'
-  function showBlanks () {
-    blank =''
-    for(let i = 0; i< hiddenWord.length;i++){
-        blank = `${blank} _`
-  
-    }
-    return blank
-  }
-  
-  showBlanks()
-console.log(blank)
 
 
-let listOfGuess = {} 
 
 function numPreviousGuessed () {
 
@@ -60,88 +108,60 @@ function numPreviousGuessed () {
 } else if(!listOfGuess.hasOwnProperty(guess)){
   listOfGuess[guess] = 1;
 }
+console.log(`Previous guesses:`, listOfGuess)
   return listOfGuess
 
 }
 numPreviousGuessed()
-console.log(listOfGuess)
+// console.log(listOfGuess)
 
 
 
 
 
 // Function to fill in guessed word
-let filler = ()=> {
-  let firstEl = ''
+function filler  () {
   let letter = guess
   let array = []
-  for(let i = 0; i<hiddenWord.length;i++){
-    if(hiddenWord[i] === letter){
+  let wordArray = hiddenWord.split('')
+  wordArray.forEach((el,i)=>{
+    if(el === letter){
      array = blank.split(' ')
-     firstEl = array.shift()
-     array[i] = guess
+     array.shift()
+     array.splice(i,1,letter)
+     array.unshift()
      blank= array.join(' ')
       } else{
-        continue
+        
       }
-    }
-   return 
+    })
+   
 }
-
 filler()
 console.log(blank)
 
 
 
 
-
-
-let numOfGuess = 10
-let counter = 0;
-
-let array = []
-
-let currentGuessCount = 10
-//Decrease guess count by 1
-let lowerByOne = ()=>{
-    if(!(hiddenWord.hasOwnProperty(guess))){
-  numOfGuess -= 1
-  if(numOfGuess >1){
-  console.log(`you have ${numOfGuess} guesses remaining.`)
-  } else{
-    console.log(`You have ${numOfGuess} guess remaining`)
-  }
-}
-}
-
   console.log(numOfGuess)
   lowerByOne()
-  console.log(numOfGuess)
+  // console.log(numOfGuess)
 
 
 
-
-  
-
-
-// decrement guess count
-for(i=0;i>numOfGuess;i--){
-  count -= 1
-}
-
-// Ends game if guess count hits zero
-if(numOfGuess === 0){
-  console.log('Sorry, you lose')
-}
 
 
 
 console.log(`Your total Number of Guesses is: ${totNumOfGuess}`)
 
-// let numOfGuess= (num)=>{
-//   if(!input))
-//   return 
 
-// }
 
-// startgame()
+let playGame =()=>{}
+
+
+let startgame = ()=>{
+  playGame()
+}
+startgame()
+}
+
