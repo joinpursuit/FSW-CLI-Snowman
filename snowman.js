b713d62180dea87ba12b71e46216eb79af63f45e
jcanizales23 <johncanizales@pursuit.org>
2021-01-17 01:15:56 -0500

    Last few add ons before R&R

--- snowman.js
+++ snowman.js
@@ -12,23 +12,38 @@ console.log("Yup! I know you've heard of it.");
 console.log("Now let's play!");
 console.log("You have 5 guesses.");
 console.log("HINT! The stars are bright. They lead with might. Look Up!");
-let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-'t', 'u', 'v', 'w', 'x', 'y', 'z'];
 // gameTakeOff
+const startGame = () => {
+  foreach.startGame(randomLetter(pickRandomLetter))
+}
 const randomLetter = () => {
   let pickRandomLetter = readlineSync.question("Pick a letter");
-  for(pickRandomLetter;  )
-}
+  let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+  'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+  't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  if(pickRandomLetter === randomWord) {
+    return true
+  } else{
+    return false
+  }
+};
 // gameWord
 const randomWord = () => {
+  let gameWord = "Libra";
+  if(randomLetter === "L", "l", "I", "i", "B", "b", "R", "r", "A", "a") {
+    return true
+  }
 
-}
+};
 // numberOfGuess
 const numOfGuess = () => {
 
-}
+};
 // guessLog
 const guessLog = () => {
 
-}
+};
+// gameOver
+const gameOver = () => {
+
+};
