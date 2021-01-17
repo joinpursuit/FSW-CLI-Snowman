32e12ae41a494c7072182297ecd7de64579d0484
jcanizales23 <johncanizales@pursuit.org>
2021-01-17 00:13:50 -0500

    ADDED dictionary and intro logs

--- snowman.js
+++ snowman.js
@@ -1,10 +1,19 @@
 // SnowMan Project
 // Intro 
 //constVariables
-const CHALK
-const 
+const readlineSync = require('readline-sync');
+const chalk = require('chalk');
+const dictionary = require("./dictionary.js");
+//Intro logs
+console.log("Welcome!");
+console.log("Today we have a game that you may be familiar with.");
+console.log("It's called Hangman.");
+console.log("Yup! I know you've heard of it.");
+console.log("Now let's play!");
+console.log("You have 5 guesses.");
+console.log("HINT! The stars are bright. They lead with might. Look Up!");
 // gameTakeOff
-
-const pickRandomLetter = () => {
-  while(wordHasNotBeenGuessed)
-}
+//const pickRandomLetter = () => {
+//  let pickRandomLetter = readlineSync.question();
+//  while(wordHasNotBeenGuessed)
+//}
