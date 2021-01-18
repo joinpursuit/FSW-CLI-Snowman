af74c98aa66a9357d860a37e4cfa153bca2873b5
Jasleen Villamil <jasleenvillamil@gmail.com>
2021-01-18 03:44:29 -0500

    Third commit. People can make letter guesses and it'll tell them if theyre right or wrong

--- snowman.js
+++ snowman.js
@@ -1,150 +1,52 @@
 const readline = require('readline-sync');
 const dictionary = ["able", "about", "account", "acid", "across", "addition", "adjustment", "advertisement", "after", "again", "against", "agreement", "almost", "among", "amount", "amusement", "angle", "angry", "animal", "answer", "apparatus", "apple", "approval", "arch", "argument", "army", "attack", "attempt", "attention", "attraction", "authority", "automatic", "awake", "baby", "back", "balance", "ball", "band", "base", "basin", "basket", "bath", "beautiful", "because", "before", "behaviour", "belief", "bell", "bent", "berry", "between", "bird", "birth", "bite", "bitter", "black", "blade", "blood", "blow", "blue", "board", "boat", "body", "boiling", "bone", "book", "boot", "bottle", "brain", "brake", "branch", "brass", "bread", "breath", "brick", "bridge", "bright", "broken", "brother", "brown", "brush", "bucket", "building", "bulb", "burn", "burst", "business", "butter", "button", "cake", "camera", "canvas", "card", "care", "carriage", "cart", "cause", "certain", "chain", "chalk", "chance", "change", "cheap", "cheese", "chemical", "chest", "chief", "chin", "church", "circle", "clean", "clear", "clock", "cloth", "cloud", "coal", "coat", "cold", "collar", "colour", "comb", "come", "comfort", "committee", "common", "company", "comparison", "competition", "complete", "complex", "condition", "connection", "conscious", "control", "cook", "copper", "copy", "cord", "cork", "cotton", "cough", "country", "cover", "crack", "credit", "crime", "cruel", "crush", "current", "curtain", "curve", "cushion", "damage", "danger", "dark", "daughter", "dead", "dear", "death", "debt", "decision", "deep", "degree", "delicate", "dependent", "design", "desire", "destruction", "detail", "development", "different", "digestion", "direction", "dirty", "discovery", "discussion", "disease", "disgust", "distance", "distribution", "division", "door", "doubt", "down", "drain", "drawer", "dress", "drink", "driving", "drop", "dust", "early", "earth", "east", "edge", "education", "effect", "elastic", "electric", "engine", "enough", "equal", "error", "even", "event", "ever", "every", "example", "exchange", "existence", "expansion", "experience", "expert", "face", "fact", "fall", "false", "family", "farm", "father", "fear", "feather", "feeble", "feeling", "female", "fertile", "fiction", "field", "fight", "finger", "fire", "first", "fish", "fixed", "flag", "flame", "flat", "flight", "floor", "flower", "fold", "food", "foolish", "foot", "force", "fork", "form", "forward", "fowl", "frame", "free", "frequent", "friend", "from", "front", "fruit", "full", "future", "garden", "general", "girl", "give", "glass", "glove", "goat", "gold", "good", "government", "grain", "grass", "great", "green", "grey", "grip", "group", "growth", "guide", "hair", "hammer", "hand", "hanging", "happy", "harbour", "hard", "harmony", "hate", "have", "head", "healthy", "hear", "hearing", "heart", "heat", "help", "high", "history", "hole", "hollow", "hook", "hope", "horn", "horse", "hospital", "hour", "house", "humour", "idea", "important", "impulse", "increase", "industry", "insect", "instrument", "insurance", "interest", "invention", "iron", "island", "jelly", "jewel", "join", "journey", "judge", "jump", "keep", "kettle", "kick", "kind", "kiss", "knee", "knife", "knot", "knowledge", "land", "language", "last", "late", "laugh", "lead", "leaf", "learning", "leather", "left", "letter", "level", "library", "lift", "light", "like", "limit", "line", "linen", "liquid", "list", "little", "living", "lock", "long", "look", "loose", "loss", "loud", "love", "machine", "make", "male", "manager", "mark", "market", "married", "mass", "match", "material", "meal", "measure", "meat", "medical", "meeting", "memory", "metal", "middle", "military", "milk", "mind", "mine", "minute", "mist", "mixed", "money", "monkey", "month", "moon", "morning", "mother", "motion", "mountain", "mouth", "move", "much", "muscle", "music", "nail", "name", "narrow", "nation", "natural", "near", "necessary", "neck", "need", "needle", "nerve", "news", "night", "noise", "normal", "north", "nose", "note", "number", "observation", "offer", "office", "only", "open", "operation", "opinion", "opposite", "orange", "order", "organization", "ornament", "other", "oven", "over", "owner", "page", "pain", "paint", "paper", "parallel", "parcel", "part", "past", "paste", "payment", "peace", "pencil", "person", "physical", "picture", "pipe", "place", "plane", "plant", "plate", "play", "please", "pleasure", "plough", "pocket", "point", "poison", "polish", "political", "poor", "porter", "position", "possible", "potato", "powder", "power", "present", "price", "print", "prison", "private", "probable", "process", "produce", "profit", "property", "prose", "protest", "public", "pull", "pump", "punishment", "purpose", "push", "quality", "question", "quick", "quiet", "quite", "rail", "rain", "range", "rate", "reaction", "reading", "ready", "reason", "receipt", "record", "regret", "regular", "relation", "religion", "representative", "request", "respect", "responsible", "rest", "reward", "rhythm", "rice", "right", "ring", "river", "road", "roll", "roof", "room", "root", "rough", "round", "rule", "safe", "sail", "salt", "same", "sand", "scale", "school", "science", "scissors", "screw", "seat", "second", "secret", "secretary", "seed", "seem", "selection", "self", "send", "sense", "separate", "serious", "servant", "shade", "shake", "shame", "sharp", "sheep", "shelf", "ship", "shirt", "shock", "shoe", "short", "shut", "side", "sign", "silk", "silver", "simple", "sister", "size", "skin", "skirt", "sleep", "slip", "slope", "slow", "small", "smash", "smell", "smile", "smoke", "smooth", "snake", "sneeze", "snow", "soap", "society", "sock", "soft", "solid", "some", "song", "sort", "sound", "soup", "south", "space", "spade", "special", "sponge", "spoon", "spring", "square", "stage", "stamp", "star", "start", "statement", "station", "steam", "steel", "stem", "step", "stick", "sticky", "stiff", "still", "stitch", "stocking", "stomach", "stone", "stop", "store", "story", "straight", "strange", "street", "stretch", "strong", "structure", "substance", "such", "sudden", "sugar", "suggestion", "summer", "support", "surprise", "sweet", "swim", "system", "table", "tail", "take", "talk", "tall", "taste", "teaching", "tendency", "test", "than", "that", "then", "theory", "there", "thick", "thin", "thing", "this", "thought", "thread", "throat", "through", "through", "thumb", "thunder", "ticket", "tight", "till", "time", "tired", "together", "tomorrow", "tongue", "tooth", "touch", "town", "trade", "train", "transport", "tray", "tree", "trick", "trouble", "trousers", "true", "turn", "twist", "umbrella", "under", "unit", "value", "verse", "very", "vessel", "view", "violent", "voice", "waiting", "walk", "wall", "warm", "wash", "waste", "watch", "water", "wave", "weather", "week", "weight", "well", "west", "wheel", "when", "where", "while", "whip", "whistle", "white", "wide", "will", "wind", "window", "wine", "wing", "winter", "wire", "wise", "with", "woman", "wood", "wool", "word", "work", "worm", "wound", "writing", "wrong", "year", "yellow", "yesterday", "young"]
 
-//Game Variables
-//chosen dictionary word
-//guessed letter
-//guess count
-//something to hold and show the guessed letters
-//the word that is shown with the _
-//
-let snowman = {
-  dictionaryWord:"",
-  hiddenWord:"",
-  guessedLetter: "",
-  guessesLeft: 6,
-  guessedLetters: [],
-  shownWord: [],
+snowman = {
+  letter: "",
+  randomWord:"",
+  randomWordArr: [],
+  blankSpaces:"",
+  blankSpacesArr: [],
+  guessCount: 6,
+  guessedLettersArr: []
 }
+
+  snowman.randomWord = dictionary[Math.round(Math.random()*(dictionary.length))]
+  snowman.randomWordArr =  snowman.randomWord.split("")
+
+console.log(snowman.randomWord)
+
+ 
+ function snowmanIntro(){
+    console.log(`Welcome to my Snowman Game! \n The goal is to guess the word the computer has randomly chosen. \n 
+    You will have 5 chances to type in a letter, but you cannot type the word.`)
+    let play = readline.keyInYN("Would you like to play?")
+    if(play) {
+      console.log("Alright, great! Let's start!")
+      getValidLetterGuess()
+      guessedLetters()
+  
+    }
+  }
 
 function getValidLetterGuess() {
-  function guessIsValid() {
-    return letter.length === 1 && letter.toUpperCase() != letter.toLowerCase()
-  }
-  let letter = ""
-  while (!letter) {
-    let input = readline.question("Please enter your guess: ")
-    if (guessIsValid(input)) {
-      letter = input
-    } else {
-      console.log("Please enter a valid letter")
-    }
-  }
-  return  letter.toLowerCase()
-}
-
-/*
-## User Stories
-
-Copy the user stories from the Trello board [here](https://trello.com/b/8OErA3xI/command-line-snowman-project-plan) into your own board.
-
-### As a user, I should be presented with a random word to guess
-
-- The computer should pick a word at random
-- The user should see a number of `_` characters equal to the number of letters in the word (ex. A _ _ L E, for apple) */
-
-//needs to take a word from the dictionary
-function randomWord(){
-  return dictionaryWord = dictionary[Math.round(Math.random()*(dictionary.length))]
   
-}
-console.log(randomWord())
-console.log(dictionaryWord)
+    let input = readline.question(` Please enter your guess: `) 
+    if (input.length === 1) {
+      snowman.letter = input 
+      snowman.guessedLettersArr.push(snowman.letter)}
+      else console.log('please enter valid letter')}
 
-//needs to use that word to show _ = letters in word
-function showLetterSpaces(letter){
-  for (let i = 0; i < snowman.dictionaryWord.length; i++) {
-    if (i === 0) {
-      snowman.hiddenWord = snowman.hiddenWord + '_'
-    }
-    else {
-      snowman.hiddenWord = snowmanObject.hiddenWord + ' _'
-    }
-  }
-  console.log(snowman.hiddenWord)
-}
-console.log(showLetterSpaces())
-
-
-/*### As a user, I should be able to enter guesses
-
-- Handle invalid guesses by displaying a message and having the user enter a different guess.  Invalid guesses don't count against the guess count.
-- After each guess, the user should see the new updated word, replacing all `_` with letters they have guessed
-- After each guess, the user should see the letters they have guessed already
-- After each incorrect guess, the number of guesses should decrease.
-- After each correct guess, the number of guesses should stay the same.*/
-
-//if guess is invalid, show error
-//if incorrect, number count decreases
-//if correct, number count stays the same
-//show letters guessed
-function guessCount(){
-  console.log(`You have ${snowman.guessesLeft} guesses remaining`)
-  if (snowman.guessesLeft === 0) {
-    console.log('You lose')
-    console.log("The word was" + dictionaryWord)
-  }
-}
-console.log(guessCount())
-
-
-//if guess is correct, update spaces with guessed letter
-function correctLetterGuess(){
-
-}
+function guessedLetters(){
+      if(snowman.randomWord.split('').includes(snowman.letter)){
+        return console.log(`You guessed correct!`)
+      } else snowman.guessCount -= 1
+      console.log (`you guessed wrong! So far you've guessed ${snowman.guessedLettersArr} and have ${snowman.guessCount} guess(es) left `)}
+    
+    
+snowmanIntro()
 
 
 
-/*### As a user, I should know how many guesses I have left
 
-- At the beginning of the game, the number of guesses remaining should be visible
-- After each guess, the updated number of guesses should be visible*/
-
-//keep guess count visible, show at begining of game
-function visibleGuessCount(){
-
-}
-
-
-
-/*### As a user, I should know when I win or lose and see the correct answer.
-- The game should continue until the user has won or lost
-- Once the full word is guessed, the game should display how many guesses it took and display a victory message
-- If the user runs out of guesses, the full word should be revealed and the game should display a defeat message */
-
-//need to determine length of game by certain amount of guesses
-//if word is guessed before count reaches zero display victory message
-function guessedCorrect(){
-
-}
-
-//game ends if guesses reach zero
-//show the full word with a defeat message
-function outOfGuesses(){
-
-}
-
-//Explain directions, guess count, ask do you want to play?
-
-function snowmanIntro(){
-  console.log(`Welcome to my Snowman Game! \n The goal is to guess the word the computer has randomly chosen. \n 
-  You will have 5 chances to type in a letter, but you cannot type the word.`)
-  let play = readline.keyInYN("Would you like to play?")
-  if(play) {
-    console.log("Alright, great! Let's start!")
-
-  }
-}
-
-
-function gameLoop() {
-  while(snowman.guessesLeft !== 0)
-  gameOver()
-}
-function startGame(){
-
-}
-//Ask do you want to play again, if not terminates game.
-function gameOver(){
-  process.exit
-}
+    
