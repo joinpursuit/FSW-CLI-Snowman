f9025b546d25ebc48d20d970fd182fcff57882da
Tamzeed Rahman <tamzeedrahman@gmail.com>
2021-01-18 20:51:09 -0500

    seriously not done part 2

--- snowman.js
+++ snowman.js
@@ -5,6 +5,7 @@ let stats = {
   totalGuesses: 8,
   guessedLetters: [], 
   trackedWord: '',
+  theWord: [],
   randomWordAns: randomWordPicker(),
 
 }
@@ -25,34 +26,14 @@ function snowManGameBeginning(){
 function randomWordPicker(){
   return dictionary[Math.floor(Math.random() * (dictionary.length ))]
 }
-function currentword(){
-  word = randomWordAns.split('') //apple [a,p,p,l,e]
-  for (let i = 0; i < randomWordAns.length; i++) {
-    stats.guessedLetters.push('_')
-  }
-  console.log(word)
-}
+
 
 function gameLoop(){
-  let guessLetter = getValidLetterGuess() //l
-  let word = stats.randomWordAns //apple
-  console.log(word) // apple
-for ( i = 0 ; i < word.length; i++){ // 0<8
-
-    stats.trackedWord += ' _'   // '' += _
+  word = stats.randomWordAns.split('') //apple [a,p,p,l,e]
+  for (let i = 0; i < word.length; i++){ //[a,p,p,l,e]
+    stats.theWord.push('_')
   }
-  console.log(stats.trackedWord) // ' ' 
-  // the guessed letter chosen if correct needs to correspond to to the index of 
-  if (word.includes(guessLetter)){ //    true 'apple'.include('l')       
-    for(i=0; i < totalGuesses; i++){         //0<5
-      if(guessLetter === word[i]){              
-        stats.trackedWord[i] = guessLetter 
-      }
-    }
-  }
-  stats.guessedLetters.push(guessLetter)
-  console.log(stats.guessedLetters)
-  console.log(stats.trackedWord)
+  console.log(word)
  
 }
 
