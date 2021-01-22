161a751e7f0138969fb892cca1d610031c7c127b
Jasleen Villamil <jasleenvillamil@gmail.com>
2021-01-22 03:09:33 -0500

    Fixed the move counter, added chalk

--- snowman.js
+++ snowman.js
@@ -1,21 +1,22 @@
 const readline = require('readline-sync');
 const dictionary = ["able", "about", "account", "acid", "across", "addition", "adjustment", "advertisement", "after", "again", "against", "agreement", "almost", "among", "amount", "amusement", "angle", "angry", "animal", "answer", "apparatus", "apple", "approval", "arch", "argument", "army", "attack", "attempt", "attention", "attraction", "authority", "automatic", "awake", "baby", "back", "balance", "ball", "band", "base", "basin", "basket", "bath", "beautiful", "because", "before", "behaviour", "belief", "bell", "bent", "berry", "between", "bird", "birth", "bite", "bitter", "black", "blade", "blood", "blow", "blue", "board", "boat", "body", "boiling", "bone", "book", "boot", "bottle", "brain", "brake", "branch", "brass", "bread", "breath", "brick", "bridge", "bright", "broken", "brother", "brown", "brush", "bucket", "building", "bulb", "burn", "burst", "business", "butter", "button", "cake", "camera", "canvas", "card", "care", "carriage", "cart", "cause", "certain", "chain", "chalk", "chance", "change", "cheap", "cheese", "chemical", "chest", "chief", "chin", "church", "circle", "clean", "clear", "clock", "cloth", "cloud", "coal", "coat", "cold", "collar", "colour", "comb", "come", "comfort", "committee", "common", "company", "comparison", "competition", "complete", "complex", "condition", "connection", "conscious", "control", "cook", "copper", "copy", "cord", "cork", "cotton", "cough", "country", "cover", "crack", "credit", "crime", "cruel", "crush", "current", "curtain", "curve", "cushion", "damage", "danger", "dark", "daughter", "dead", "dear", "death", "debt", "decision", "deep", "degree", "delicate", "dependent", "design", "desire", "destruction", "detail", "development", "different", "digestion", "direction", "dirty", "discovery", "discussion", "disease", "disgust", "distance", "distribution", "division", "door", "doubt", "down", "drain", "drawer", "dress", "drink", "driving", "drop", "dust", "early", "earth", "east", "edge", "education", "effect", "elastic", "electric", "engine", "enough", "equal", "error", "even", "event", "ever", "every", "example", "exchange", "existence", "expansion", "experience", "expert", "face", "fact", "fall", "false", "family", "farm", "father", "fear", "feather", "feeble", "feeling", "female", "fertile", "fiction", "field", "fight", "finger", "fire", "first", "fish", "fixed", "flag", "flame", "flat", "flight", "floor", "flower", "fold", "food", "foolish", "foot", "force", "fork", "form", "forward", "fowl", "frame", "free", "frequent", "friend", "from", "front", "fruit", "full", "future", "garden", "general", "girl", "give", "glass", "glove", "goat", "gold", "good", "government", "grain", "grass", "great", "green", "grey", "grip", "group", "growth", "guide", "hair", "hammer", "hand", "hanging", "happy", "harbour", "hard", "harmony", "hate", "have", "head", "healthy", "hear", "hearing", "heart", "heat", "help", "high", "history", "hole", "hollow", "hook", "hope", "horn", "horse", "hospital", "hour", "house", "humour", "idea", "important", "impulse", "increase", "industry", "insect", "instrument", "insurance", "interest", "invention", "iron", "island", "jelly", "jewel", "join", "journey", "judge", "jump", "keep", "kettle", "kick", "kind", "kiss", "knee", "knife", "knot", "knowledge", "land", "language", "last", "late", "laugh", "lead", "leaf", "learning", "leather", "left", "letter", "level", "library", "lift", "light", "like", "limit", "line", "linen", "liquid", "list", "little", "living", "lock", "long", "look", "loose", "loss", "loud", "love", "machine", "make", "male", "manager", "mark", "market", "married", "mass", "match", "material", "meal", "measure", "meat", "medical", "meeting", "memory", "metal", "middle", "military", "milk", "mind", "mine", "minute", "mist", "mixed", "money", "monkey", "month", "moon", "morning", "mother", "motion", "mountain", "mouth", "move", "much", "muscle", "music", "nail", "name", "narrow", "nation", "natural", "near", "necessary", "neck", "need", "needle", "nerve", "news", "night", "noise", "normal", "north", "nose", "note", "number", "observation", "offer", "office", "only", "open", "operation", "opinion", "opposite", "orange", "order", "organization", "ornament", "other", "oven", "over", "owner", "page", "pain", "paint", "paper", "parallel", "parcel", "part", "past", "paste", "payment", "peace", "pencil", "person", "physical", "picture", "pipe", "place", "plane", "plant", "plate", "play", "please", "pleasure", "plough", "pocket", "point", "poison", "polish", "political", "poor", "porter", "position", "possible", "potato", "powder", "power", "present", "price", "print", "prison", "private", "probable", "process", "produce", "profit", "property", "prose", "protest", "public", "pull", "pump", "punishment", "purpose", "push", "quality", "question", "quick", "quiet", "quite", "rail", "rain", "range", "rate", "reaction", "reading", "ready", "reason", "receipt", "record", "regret", "regular", "relation", "religion", "representative", "request", "respect", "responsible", "rest", "reward", "rhythm", "rice", "right", "ring", "river", "road", "roll", "roof", "room", "root", "rough", "round", "rule", "safe", "sail", "salt", "same", "sand", "scale", "school", "science", "scissors", "screw", "seat", "second", "secret", "secretary", "seed", "seem", "selection", "self", "send", "sense", "separate", "serious", "servant", "shade", "shake", "shame", "sharp", "sheep", "shelf", "ship", "shirt", "shock", "shoe", "short", "shut", "side", "sign", "silk", "silver", "simple", "sister", "size", "skin", "skirt", "sleep", "slip", "slope", "slow", "small", "smash", "smell", "smile", "smoke", "smooth", "snake", "sneeze", "snow", "soap", "society", "sock", "soft", "solid", "some", "song", "sort", "sound", "soup", "south", "space", "spade", "special", "sponge", "spoon", "spring", "square", "stage", "stamp", "star", "start", "statement", "station", "steam", "steel", "stem", "step", "stick", "sticky", "stiff", "still", "stitch", "stocking", "stomach", "stone", "stop", "store", "story", "straight", "strange", "street", "stretch", "strong", "structure", "substance", "such", "sudden", "sugar", "suggestion", "summer", "support", "surprise", "sweet", "swim", "system", "table", "tail", "take", "talk", "tall", "taste", "teaching", "tendency", "test", "than", "that", "then", "theory", "there", "thick", "thin", "thing", "this", "thought", "thread", "throat", "through", "through", "thumb", "thunder", "ticket", "tight", "till", "time", "tired", "together", "tomorrow", "tongue", "tooth", "touch", "town", "trade", "train", "transport", "tray", "tree", "trick", "trouble", "trousers", "true", "turn", "twist", "umbrella", "under", "unit", "value", "verse", "very", "vessel", "view", "violent", "voice", "waiting", "walk", "wall", "warm", "wash", "waste", "watch", "water", "wave", "weather", "week", "weight", "well", "west", "wheel", "when", "where", "while", "whip", "whistle", "white", "wide", "will", "wind", "window", "wine", "wing", "winter", "wire", "wise", "with", "woman", "wood", "wool", "word", "work", "worm", "wound", "writing", "wrong", "year", "yellow", "yesterday", "young"]
-
+const chalk = require('chalk')
 function startGame (){
- console.log(`\nWelcome to Snowman! \n 
-All you need to do, is guess the letters of a  word, randomly chosen. \n
+ console.log(chalk.blue(`\nWelcome to Snowman! \n`))
+console.log(`All you need to do, is guess the letters of a word, randomly chosen. \n
 You will have 7 chances to type in a letter, but you cannot type the word. \n
-For every letter you guess incorrectly, you lose a chance.`) 
+For every letter you guess incorrectly, you lose a chance.`)
   randomWord()
   getValidLetterGuess()
 }
 
-let moves = 0
+
 gameboard = {
   word:[],
   letters: "",
   guesses: 7 ,
-  guessedWord: []
+  guessedWord: [],
+  moves: 0
 }
 
 
@@ -25,11 +26,22 @@ function randomWord(){
   for (let i of randomWord){
     gameboard.guessedWord.push('_')
   }
+
 }
-    
+function replaceLetter (letter) {
+  let sameIndexOfOriginalWord
+  for(let i = 0; i < gameboard.word.length; i +=1) {
+    if(gameboard.word[i]=== letter){
+      sameIndexOfOriginalWord = i
+      gameboard.guessedWord.splice(sameIndexOfOriginalWord,1,letter)
+    }
+  }
+ 
+}
+
   
 function getValidLetterGuess() {
-  console.log(`\n ${gameboard.guessedWord.join(' ')}\n` )
+  console.log(chalk.blue(`\n ${gameboard.guessedWord.join(' ')}\n` ))
   console.log(`Used Letters: ${gameboard.letters}\nChances Left: ${gameboard.guesses}`)
 
   function guessIsValid(letter) {
@@ -37,13 +49,13 @@ function getValidLetterGuess() {
 }
 let letter = ""
   while (!letter){ 
-    let input = readline.question(` \nGive me a letter between A-Z: `) 
+    let input = readline.question(chalk.blue(` \nGive me a letter between A-Z: `))
     if (guessIsValid(input) && !(gameboard.letters.includes(input))){
       letter = input 
       gameboard.letters += `${letter},`
       guessedLetters(letter)
     } else { 
-      console.log('\n\tPlease enter valid letter!')
+      console.log(chalk.keyword('orange')('\n\tPlease enter valid letter!'))
   }
 }return letter.toLowerCase()
 }
@@ -51,51 +63,41 @@ let letter = ""
 function guessedLetters (letter) {
 
       if (gameboard.word.includes(letter)){
-        console.log(`\n\tLooks like that word does have ${letter} in there. Good job!`)
+        console.log(chalk.green(`\n\tLooks like that word does have ${letter} in there. Good job!`))
         replaceLetter(letter)
-        moves +=1
+        gameboard.moves +=1
         if (gameboard.word.join('') === gameboard.guessedWord.join('')) {
         youWin()
       }
         getValidLetterGuess()
-      } else console.log (`\n\tLooks like there's no ${letter}!\n`)
+      } else {
+        console.log (chalk.keyword('orange')(`\n\tLooks like there's no ${letter}!\n`))
       gameboard.guesses --
-      moves +=1
-      if(gameboard.guesses === 0) {
+      gameboard.moves +=1
+    if(gameboard.guesses === 0) {
       youLose()  
       }
+      } 
       getValidLetterGuess()
     }
       
 
-    function replaceLetter (letter) {
-      let index
-      for(let i = 0; i < gameboard.word.length; i += 1) {
-        if(gameboard.word[i]=== letter){
-          index = i
-          gameboard.guessedWord.splice(index,1,letter)
-        }
-      }
-     
-    }
     
 
-
 function youWin (){
-
-console.log(`\n\b\tYou win! It took you ${moves} guesses to solve the word "${gameboard.word.join('')}"\n` )
+console.log(chalk.green(`\n\b\tYou win! It took you ${gameboard.moves} guesses to solve the word "${gameboard.word.join('')}"\n` ))
 gameOver()
 }
 
 function youLose(){
-  console.log(`\n\bOh no, you ran out of chances! The word was ${gameboard.word.join('')}!"\n` )
+  console.log(chalk.bold.red(`\n\bOh no, you ran out of chances! The word was ${gameboard.word.join('')}!"\n` ))
 gameOver()
 }
 
 function gameOver(){
   let startOver = readline.keyInYNStrict(`Do you want to play again?`)
   if(startOver) {
-    let moves = 0
+    gameboard.moves = 0
     gameboard.guesses = 7
     gameboard.letters = ""
     gameboard.word = []
