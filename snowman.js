ccab27d18e0df51b1baf1222c8f530dd9c39291d
Coreen Cooper <coreencooper@pursuit.org>
2021-01-25 07:47:05 -0500

    include npm chalk

--- snowman.js
+++ snowman.js
@@ -1,105 +1,125 @@
 "use strict";
-const readline = require('readline-sync');
-const dictionary = require('./dictionary.js');
+const readline = require("readline-sync");
+const chalk = require("chalk");
+const dictionary = require("./dictionary.js");
 let word = dictionary[Math.floor(Math.random() * dictionary.length)];
 let wordSplit = word.split("");
-let board = displayUnderscores()
+let board = displayUnderscores();
 let gameDataObj = {};
 let guessCountDecrement = 7;
 let turnTaken = 1;
-// console.log(word)
-
 
 ////////////////validate user input ///////////////////// default code ////
 function getValidLetterGuess() {
   function guessIsValid(letter) {
-    return letter.length === 1 && letter.toUpperCase() !== letter.toLowerCase() && !Object.values(gameDataObj).includes(letter)
+    return (
+      letter.length === 1 &&
+      letter.toUpperCase() !== letter.toLowerCase() &&
+      !Object.values(gameDataObj).includes(letter)
+    );
   }
-  let letter = ""
+  let letter = "";
   while (!letter) {
-    console.log(`\n${board.join(" ")}\n`) // display board
-    let input = readline.question("Please enter your guess: ")
+    console.log(`\n${board.join(" ")}\n`); // display board
+    let input = readline.question("Please enter your guess: ");
     if (guessIsValid(input)) {
-      letter = input
-      console.log(displayLettersGuessed(letter)) // display user input
-      compareLetters(letter) // comapre if user input is in secret word
+      letter = input;
+      console.log(displayLettersGuessed(letter)); // display user input
+      compareLetters(letter); // comapre if user input is in secret word
     } else {
-      console.clear()
-      console.log(displayLettersGuessed(letter)) // display user input
-      console.log("Please enter a valid letter")
+      console.clear();
+      console.log(displayLettersGuessed(letter)); // display user input
+      console.log(chalk.red("Please enter a valid letter"));
     }
   }
-  return letter.toLowerCase() 
+  return letter.toLowerCase();
 }
 
 //////////////////// compareLetters //////////////////////
-function compareLetters (userInput) {
+function compareLetters(userInput) {
   let isGuessCorrect = false;
   for (let i = 0; i < wordSplit.length; i++) {
-     if (userInput.toLowerCase() === wordSplit[i]) {
-      board[i] = wordSplit[i]
+    if (userInput.toLowerCase() === wordSplit[i]) {
+      board[i] = wordSplit[i];
       isGuessCorrect = true;
-    } 
-  } 
+    }
+  }
   if (isGuessCorrect === false) {
-    guessCountDecrement--
+    guessCountDecrement--;
   }
 }
 
 ////////////// check for win ////////////////
-const isBoardFull = () => !board.includes("_") 
+const isBoardFull = () => !board.includes("_");
 
 ///////////// log of user guesses in an object /////////////////////
 const displayLettersGuessed = (userInput) => {
   if (userInput) {
-    gameDataObj[turnTaken] = userInput.toLowerCase()
-      turnTaken++
-    }
-    console.clear()
-    return `Letters guessed: ${Object.values(gameDataObj).join(", ")} `
+    gameDataObj[turnTaken] = userInput.toLowerCase();
+    turnTaken++;
   }
+  console.clear();
+  return (
+    chalk.yellow("Letters guessed:") +
+    ` ${Object.values(gameDataObj).join(", ")} `
+  );
+};
 
 ///////////////////// create a board with blank spaces ////////////////////////// ES5 hoisting
-function displayUnderscores() { 
+function displayUnderscores() {
   return wordSplit.map((el) => {
     return (el = "_");
   });
-};
+}
 
 //////////////////// display guess count and win lose message //////////////////////////
 const printGuessCount = () => {
-  console.clear()
-  console.log("")
-  while(!isBoardFull()) {
+  console.clear();
+  console.log("");
+  while (!isBoardFull()) {
     if (guessCountDecrement === 7) {
-        console.log(`You have 7 guesses total`)
+      console.log("You have " + chalk.green(7) + " guesses total");
     } else if (guessCountDecrement > 1) {
-        console.log(`You have ${guessCountDecrement} guesses left`)
+      console.log(
+        "You have " + chalk.green(guessCountDecrement) + " guesses left"
+      );
     } else if (guessCountDecrement === 1) {
-        console.log(`You have ${guessCountDecrement} guess left`)
+      console.log("You have " + chalk.red(guessCountDecrement) + " guess left");
     } else {
-        console.log(`\nSorry. You lose\n\nThe word was: ${word}`)
-        quitGame();
+      console.log(
+        chalk.red("\nSorry. You lose") +
+          "\n\nThe word was: " +
+          chalk.yellow(word)
+      );
+      quitGame();
     }
-    getValidLetterGuess()
-}
-return console.log(`\n${board.join(" ")}\n\nYou're Brilliant! You Won!\n\nIt took ${winCount(turnTaken)} guesses.`)
+    getValidLetterGuess();
+  }
+  return console.log(
+    "\n" +
+      chalk.rgb(255, 165, 0)(board.join(" ")) +
+      chalk.green("\n\nYou're Brilliant! You Won!") +
+      "\n\nIt took " +
+      chalk.green(winCount(turnTaken)) +
+      " guesses."
+  );
 };
 
 ///////////////// tally total guesses used //////////////////////////////
-const winCount = turnTaken => turnTaken  - 1;
+const winCount = (turnTaken) => turnTaken - 1;
 
 //////////////// end game //////////////////
-const quitGame = () => process.exit()
-
+const quitGame = () => process.exit();
 
 ///////////// user choose to start game ///////////////////
 const startGame = () => {
-  console.clear()
-    return readline.keyInYNStrict("Welcome!\n\nWould you like to play The Snowman Game?") ? printGuessCount() : console.log("\nYou're no fun.")
-  };
+  console.clear();
+  // console.log(word)
+  return readline.keyInYNStrict(
+    chalk.yellow("Welcome!\n\nWould you like to play The Snowman Game?")
+  )
+    ? printGuessCount()
+    : console.log(chalk.red("\nYou're no fun."));
+};
 
 startGame();
-
-
-
