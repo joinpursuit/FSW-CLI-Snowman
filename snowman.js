8d6dd151e94ec49e904274412263ca6c5b983e25
Maddy <74828969+pursuitMaddy@users.noreply.github.com>
2021-01-25 01:33:30 -0500

    need 2 more things

--- snowman.js
+++ snowman.js
@@ -1,7 +1,6 @@
 const readline = require('readline-sync');
 const dictionary = ["able", "about", "account", "acid", "across", "addition", "adjustment", "advertisement", "after", "again", "against", "agreement", "almost", "among", "amount", "amusement", "angle", "angry", "animal", "answer", "apparatus", "apple", "approval", "arch", "argument", "army", "attack", "attempt", "attention", "attraction", "authority", "automatic", "awake", "baby", "back", "balance", "ball", "band", "base", "basin", "basket", "bath", "beautiful", "because", "before", "behaviour", "belief", "bell", "bent", "berry", "between", "bird", "birth", "bite", "bitter", "black", "blade", "blood", "blow", "blue", "board", "boat", "body", "boiling", "bone", "book", "boot", "bottle", "brain", "brake", "branch", "brass", "bread", "breath", "brick", "bridge", "bright", "broken", "brother", "brown", "brush", "bucket", "building", "bulb", "burn", "burst", "business", "butter", "button", "cake", "camera", "canvas", "card", "care", "carriage", "cart", "cause", "certain", "chain", "chalk", "chance", "change", "cheap", "cheese", "chemical", "chest", "chief", "chin", "church", "circle", "clean", "clear", "clock", "cloth", "cloud", "coal", "coat", "cold", "collar", "colour", "comb", "come", "comfort", "committee", "common", "company", "comparison", "competition", "complete", "complex", "condition", "connection", "conscious", "control", "cook", "copper", "copy", "cord", "cork", "cotton", "cough", "country", "cover", "crack", "credit", "crime", "cruel", "crush", "current", "curtain", "curve", "cushion", "damage", "danger", "dark", "daughter", "dead", "dear", "death", "debt", "decision", "deep", "degree", "delicate", "dependent", "design", "desire", "destruction", "detail", "development", "different", "digestion", "direction", "dirty", "discovery", "discussion", "disease", "disgust", "distance", "distribution", "division", "door", "doubt", "down", "drain", "drawer", "dress", "drink", "driving", "drop", "dust", "early", "earth", "east", "edge", "education", "effect", "elastic", "electric", "engine", "enough", "equal", "error", "even", "event", "ever", "every", "example", "exchange", "existence", "expansion", "experience", "expert", "face", "fact", "fall", "false", "family", "farm", "father", "fear", "feather", "feeble", "feeling", "female", "fertile", "fiction", "field", "fight", "finger", "fire", "first", "fish", "fixed", "flag", "flame", "flat", "flight", "floor", "flower", "fold", "food", "foolish", "foot", "force", "fork", "form", "forward", "fowl", "frame", "free", "frequent", "friend", "from", "front", "fruit", "full", "future", "garden", "general", "girl", "give", "glass", "glove", "goat", "gold", "good", "government", "grain", "grass", "great", "green", "grey", "grip", "group", "growth", "guide", "hair", "hammer", "hand", "hanging", "happy", "harbour", "hard", "harmony", "hate", "have", "head", "healthy", "hear", "hearing", "heart", "heat", "help", "high", "history", "hole", "hollow", "hook", "hope", "horn", "horse", "hospital", "hour", "house", "humour", "idea", "important", "impulse", "increase", "industry", "insect", "instrument", "insurance", "interest", "invention", "iron", "island", "jelly", "jewel", "join", "journey", "judge", "jump", "keep", "kettle", "kick", "kind", "kiss", "knee", "knife", "knot", "knowledge", "land", "language", "last", "late", "laugh", "lead", "leaf", "learning", "leather", "left", "letter", "level", "library", "lift", "light", "like", "limit", "line", "linen", "liquid", "list", "little", "living", "lock", "long", "look", "loose", "loss", "loud", "love", "machine", "make", "male", "manager", "mark", "market", "married", "mass", "match", "material", "meal", "measure", "meat", "medical", "meeting", "memory", "metal", "middle", "military", "milk", "mind", "mine", "minute", "mist", "mixed", "money", "monkey", "month", "moon", "morning", "mother", "motion", "mountain", "mouth", "move", "much", "muscle", "music", "nail", "name", "narrow", "nation", "natural", "near", "necessary", "neck", "need", "needle", "nerve", "news", "night", "noise", "normal", "north", "nose", "note", "number", "observation", "offer", "office", "only", "open", "operation", "opinion", "opposite", "orange", "order", "organization", "ornament", "other", "oven", "over", "owner", "page", "pain", "paint", "paper", "parallel", "parcel", "part", "past", "paste", "payment", "peace", "pencil", "person", "physical", "picture", "pipe", "place", "plane", "plant", "plate", "play", "please", "pleasure", "plough", "pocket", "point", "poison", "polish", "political", "poor", "porter", "position", "possible", "potato", "powder", "power", "present", "price", "print", "prison", "private", "probable", "process", "produce", "profit", "property", "prose", "protest", "public", "pull", "pump", "punishment", "purpose", "push", "quality", "question", "quick", "quiet", "quite", "rail", "rain", "range", "rate", "reaction", "reading", "ready", "reason", "receipt", "record", "regret", "regular", "relation", "religion", "representative", "request", "respect", "responsible", "rest", "reward", "rhythm", "rice", "right", "ring", "river", "road", "roll", "roof", "room", "root", "rough", "round", "rule", "safe", "sail", "salt", "same", "sand", "scale", "school", "science", "scissors", "screw", "seat", "second", "secret", "secretary", "seed", "seem", "selection", "self", "send", "sense", "separate", "serious", "servant", "shade", "shake", "shame", "sharp", "sheep", "shelf", "ship", "shirt", "shock", "shoe", "short", "shut", "side", "sign", "silk", "silver", "simple", "sister", "size", "skin", "skirt", "sleep", "slip", "slope", "slow", "small", "smash", "smell", "smile", "smoke", "smooth", "snake", "sneeze", "snow", "soap", "society", "sock", "soft", "solid", "some", "song", "sort", "sound", "soup", "south", "space", "spade", "special", "sponge", "spoon", "spring", "square", "stage", "stamp", "star", "start", "statement", "station", "steam", "steel", "stem", "step", "stick", "sticky", "stiff", "still", "stitch", "stocking", "stomach", "stone", "stop", "store", "story", "straight", "strange", "street", "stretch", "strong", "structure", "substance", "such", "sudden", "sugar", "suggestion", "summer", "support", "surprise", "sweet", "swim", "system", "table", "tail", "take", "talk", "tall", "taste", "teaching", "tendency", "test", "than", "that", "then", "theory", "there", "thick", "thin", "thing", "this", "thought", "thread", "throat", "through", "through", "thumb", "thunder", "ticket", "tight", "till", "time", "tired", "together", "tomorrow", "tongue", "tooth", "touch", "town", "trade", "train", "transport", "tray", "tree", "trick", "trouble", "trousers", "true", "turn", "twist", "umbrella", "under", "unit", "value", "verse", "very", "vessel", "view", "violent", "voice", "waiting", "walk", "wall", "warm", "wash", "waste", "watch", "water", "wave", "weather", "week", "weight", "well", "west", "wheel", "when", "where", "while", "whip", "whistle", "white", "wide", "will", "wind", "window", "wine", "wing", "winter", "wire", "wise", "with", "woman", "wood", "wool", "word", "work", "worm", "wound", "writing", "wrong", "year", "yellow", "yesterday", "young"]
 let playerGuess = "";
-let guessTrial = 0;
 let count = 10;
 let dashSecret = "";
 let gameStatus = false;
@@ -9,7 +8,7 @@ let incorrectLetter = [];
 let correctLetter = [];
 let activeWord = [];
 let guessCount = 0;
-let winningCount = 0;
+let guessedLettersArray = [];
 
 const play = () => {
   console.log(`Thank you playing Snowman game.`);
@@ -18,56 +17,36 @@ const play = () => {
     startGame();
   } else {
     leaveGame();
-
   }
-  const leaveGame = () => {
-    console.log("Thanks for playing!")
-    Process.exit();
-  }
-
 }
+
 play();
+
+const leaveGame = () => {
+  console.log("Thanks for playing!")
+  process.exit();
+}
+
 
 function displayWordInDash(word) {
   console.log(`Please enter the letter guessed ${word}`);
 }
 
-// function winningGame() {
-//   if (!gameStatus)
-//     return;
-//   for (let a = 0; a < dashSecret.length; a++) {
-//     if (dashSecret[a] === dashSecret.length) {
-//       gameStatus = true;
-//       winningCount += 1
-//     }
-//   }
-// }
-
-
 function startGame() {
-
-
-  console.clear()
   activeWord = dictionary[Math.floor(Math.random() * dictionary.length)];
   makeBoard();
-  console.log(count)
   while (!gameStatus) {
     console.log(activeWord);
-    displayWordInDash(dashSecret.join(""));
+    displayWordInDash(dashSecret.join(" "));
     console.log(`You have ${guessCount1()} remaining!`)
-    console.log(` You have tried ${guessCount} times`);
-    //console.log(`${playerGuess} is a Correct letter!`);
-    console.log(`You already guessed ${incorrectLetter}`)
-    console.log(incorrectLetter)
-    console.log(correctLetter)
-    //console.log(`You won ${winningCount} times`)
+    console.log(`You have tried ${guessCount} times`);
+    console.log(`You've guessed wrong: ${incorrectLetter}\nYou've guessed right: ${correctLetter}`)
+    console.log(`Letter array${guessedLettersArray}`)
+    console.log(`Letter incorrect ${incorrectLetter}`);
+    console.log(`Letter correct ${correctLetter}`);
     playerGuess = getValidLetterGuess();
+    console.clear();
     replaceDash();
-    //didWin();
-    //updateGameboard();
-
-    //getValidLetterGuess();
-    //correctGuessLetter();
   }
   console.clear();
 }
@@ -78,58 +57,54 @@ function guessCount1() {
     if (el !== correctLetter) {
       counter -= 1
     } if (counter === 0) {
-      console.log(`Sorry you have tried too many times!\n The actual word is ${activeWord}`)
+      console.log(`Sorry you have tried too many times!\nThe actual word is ${activeWord}`)
       process.exit();
     }
   })
   return counter;
 }
 
+function removeDubLetter() {
+  for (let o = 0; o < activeWord.length; o++) {
+    const el = activeWord[i];
+    if (el === playerGuess) {
+      guessedLettersArray.push(el)
+    }
+    if (!activeWord.includes(playerGuess)) {
+      guessedLettersArray.push(playerGuess);
+    }
+    // return guessedLettersArray;
+  }
+}
+removeDubLetter()
 
+// const guessedLetters = () => {
+//   // for (let i = 0; i < activeWord.length; i++) {
+//   //   const el = activeWord[i];
+//   //   if (el === playerGuess && !incorrectLetter.includes(el) && !correctLetter.includes(el)) {
+//   //     guessedLettersArray.push(el);
+//   //   }
+//   // }
 
-
-
+// }
+//guessedLetters()
 
 
 function didWin() {
   if (!gameStatus);
-  //playerGuessIncludes();
   if (dashSecret.indexOf("_") < 0) {
-    //console.clear()
     console.log(`Great job you reached the end!`)
-    //console.log(readline.keyIn(`Thank you for playing YOU WON!`))
-
     process.exit()
   }
 }
 
-// function reset() {
-//   console.clear(incorrectLetter);
-//   console.clear(correctLetter);
-//   console.clear(guessCount);
-// }
-// reset()
 
 function makeBoard() {
   dashSecret = [];
   for (let a = 0; a < activeWord.length; a++) {
     dashSecret.push("_");
   }
-  // console.log(` you have already used  ${incorrectLetter + "+"}`);
-  //console.log(` You have tried ${guessTrial} to find the word`)
-  //console.log(`You have ${guessCount} Remaining!`)
 }
-
-
-// function updateGameboard() {
-//   for (let i = 0; i < activeWord.length; i++) {
-//     if (activeWord[i] === playerGuess) {
-//       console.log("checked");
-//       correctLetter.push(playerGuess);
-//       dashSecret[i] = playerGuess;
-//     }
-//   }
-// }
 
 
 function incorrectLetterGuess() {
@@ -144,10 +119,8 @@ function incorrectLetterGuess() {
 function playerGuessIncludes() {
   if (!activeWord.includes(playerGuess)) {
     incorrectLetter.push(playerGuess);
+    //guessedLettersArray.push(playerGuess)
   }
-  //return incorrectLetter;
-
-  //guessCount1()
 }
 
 
@@ -156,6 +129,7 @@ function replaceDash() {
     let currentLetter = activeWord[p];
     if (currentLetter === playerGuess) {
       correctLetter.push(currentLetter);
+      //guessedLettersArray.push(correctLetter)
       console.log(`${playerGuess} is correct!`);
       dashSecret[p] = playerGuess;
     }
@@ -166,30 +140,9 @@ function replaceDash() {
 }
 
 
-// } else {
-//   guessCount++
-
-
-// } else if (currentLetter !== playerGuess) {
-//   //if (guessCount < 10) {
-//   console.log(`Sorry,${playerGuess} is incorrect!`)
-//   if (!incorrectLetter.includes(playerGuess)) {
-
-//     incorrectLetter.push(playerGuess)
-//   }
-//incorrectLetter++
-//incorrectLetter[p] = playerGuess
-
-// } else {
-//   winningCount();
-//   if (!winningGame())
-//     console.log(`Sorry you have ${guessCount} remaining`)
-
-//console.clear();
 function getValidLetterGuess() {
   function guessIsValid(letter) {
     return letter.length === 1 && letter.toUpperCase() != letter.toLowerCase()
-
   }
   let letter = ""
   while (!letter) {
@@ -200,7 +153,6 @@ function getValidLetterGuess() {
       console.log("Please enter a valid letter")
     }
   }
-
   return letter.toLowerCase();
 }
 
