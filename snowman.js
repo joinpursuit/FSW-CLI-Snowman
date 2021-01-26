8f81e475438b5bdb364c9c6515a503de872a95b8
Raymond Udeogu <rfizzle25@gmail.com>
2021-01-26 11:19:46 -0500

    have game loop

--- snowman.js
+++ snowman.js
@@ -2,156 +2,89 @@ const readline = require('readline-sync');
 const dictionary = ["able", "about", "account", "acid", "across", "addition", "adjustment", "advertisement", "after", "again", "against", "agreement", "almost", "among", "amount", "amusement", "angle", "angry", "animal", "answer", "apparatus", "apple", "approval", "arch", "argument", "army", "attack", "attempt", "attention", "attraction", "authority", "automatic", "awake", "baby", "back", "balance", "ball", "band", "base", "basin", "basket", "bath", "beautiful", "because", "before", "behaviour", "belief", "bell", "bent", "berry", "between", "bird", "birth", "bite", "bitter", "black", "blade", "blood", "blow", "blue", "board", "boat", "body", "boiling", "bone", "book", "boot", "bottle", "brain", "brake", "branch", "brass", "bread", "breath", "brick", "bridge", "bright", "broken", "brother", "brown", "brush", "bucket", "building", "bulb", "burn", "burst", "business", "butter", "button", "cake", "camera", "canvas", "card", "care", "carriage", "cart", "cause", "certain", "chain", "chalk", "chance", "change", "cheap", "cheese", "chemical", "chest", "chief", "chin", "church", "circle", "clean", "clear", "clock", "cloth", "cloud", "coal", "coat", "cold", "collar", "colour", "comb", "come", "comfort", "committee", "common", "company", "comparison", "competition", "complete", "complex", "condition", "connection", "conscious", "control", "cook", "copper", "copy", "cord", "cork", "cotton", "cough", "country", "cover", "crack", "credit", "crime", "cruel", "crush", "current", "curtain", "curve", "cushion", "damage", "danger", "dark", "daughter", "dead", "dear", "death", "debt", "decision", "deep", "degree", "delicate", "dependent", "design", "desire", "destruction", "detail", "development", "different", "digestion", "direction", "dirty", "discovery", "discussion", "disease", "disgust", "distance", "distribution", "division", "door", "doubt", "down", "drain", "drawer", "dress", "drink", "driving", "drop", "dust", "early", "earth", "east", "edge", "education", "effect", "elastic", "electric", "engine", "enough", "equal", "error", "even", "event", "ever", "every", "example", "exchange", "existence", "expansion", "experience", "expert", "face", "fact", "fall", "false", "family", "farm", "father", "fear", "feather", "feeble", "feeling", "female", "fertile", "fiction", "field", "fight", "finger", "fire", "first", "fish", "fixed", "flag", "flame", "flat", "flight", "floor", "flower", "fold", "food", "foolish", "foot", "force", "fork", "form", "forward", "fowl", "frame", "free", "frequent", "friend", "from", "front", "fruit", "full", "future", "garden", "general", "girl", "give", "glass", "glove", "goat", "gold", "good", "government", "grain", "grass", "great", "green", "grey", "grip", "group", "growth", "guide", "hair", "hammer", "hand", "hanging", "happy", "harbour", "hard", "harmony", "hate", "have", "head", "healthy", "hear", "hearing", "heart", "heat", "help", "high", "history", "hole", "hollow", "hook", "hope", "horn", "horse", "hospital", "hour", "house", "humour", "idea", "important", "impulse", "increase", "industry", "insect", "instrument", "insurance", "interest", "invention", "iron", "island", "jelly", "jewel", "join", "journey", "judge", "jump", "keep", "kettle", "kick", "kind", "kiss", "knee", "knife", "knot", "knowledge", "land", "language", "last", "late", "laugh", "lead", "leaf", "learning", "leather", "left", "letter", "level", "library", "lift", "light", "like", "limit", "line", "linen", "liquid", "list", "little", "living", "lock", "long", "look", "loose", "loss", "loud", "love", "machine", "make", "male", "manager", "mark", "market", "married", "mass", "match", "material", "meal", "measure", "meat", "medical", "meeting", "memory", "metal", "middle", "military", "milk", "mind", "mine", "minute", "mist", "mixed", "money", "monkey", "month", "moon", "morning", "mother", "motion", "mountain", "mouth", "move", "much", "muscle", "music", "nail", "name", "narrow", "nation", "natural", "near", "necessary", "neck", "need", "needle", "nerve", "news", "night", "noise", "normal", "north", "nose", "note", "number", "observation", "offer", "office", "only", "open", "operation", "opinion", "opposite", "orange", "order", "organization", "ornament", "other", "oven", "over", "owner", "page", "pain", "paint", "paper", "parallel", "parcel", "part", "past", "paste", "payment", "peace", "pencil", "person", "physical", "picture", "pipe", "place", "plane", "plant", "plate", "play", "please", "pleasure", "plough", "pocket", "point", "poison", "polish", "political", "poor", "porter", "position", "possible", "potato", "powder", "power", "present", "price", "print", "prison", "private", "probable", "process", "produce", "profit", "property", "prose", "protest", "public", "pull", "pump", "punishment", "purpose", "push", "quality", "question", "quick", "quiet", "quite", "rail", "rain", "range", "rate", "reaction", "reading", "ready", "reason", "receipt", "record", "regret", "regular", "relation", "religion", "representative", "request", "respect", "responsible", "rest", "reward", "rhythm", "rice", "right", "ring", "river", "road", "roll", "roof", "room", "root", "rough", "round", "rule", "safe", "sail", "salt", "same", "sand", "scale", "school", "science", "scissors", "screw", "seat", "second", "secret", "secretary", "seed", "seem", "selection", "self", "send", "sense", "separate", "serious", "servant", "shade", "shake", "shame", "sharp", "sheep", "shelf", "ship", "shirt", "shock", "shoe", "short", "shut", "side", "sign", "silk", "silver", "simple", "sister", "size", "skin", "skirt", "sleep", "slip", "slope", "slow", "small", "smash", "smell", "smile", "smoke", "smooth", "snake", "sneeze", "snow", "soap", "society", "sock", "soft", "solid", "some", "song", "sort", "sound", "soup", "south", "space", "spade", "special", "sponge", "spoon", "spring", "square", "stage", "stamp", "star", "start", "statement", "station", "steam", "steel", "stem", "step", "stick", "sticky", "stiff", "still", "stitch", "stocking", "stomach", "stone", "stop", "store", "story", "straight", "strange", "street", "stretch", "strong", "structure", "substance", "such", "sudden", "sugar", "suggestion", "summer", "support", "surprise", "sweet", "swim", "system", "table", "tail", "take", "talk", "tall", "taste", "teaching", "tendency", "test", "than", "that", "then", "theory", "there", "thick", "thin", "thing", "this", "thought", "thread", "throat", "through", "through", "thumb", "thunder", "ticket", "tight", "till", "time", "tired", "together", "tomorrow", "tongue", "tooth", "touch", "town", "trade", "train", "transport", "tray", "tree", "trick", "trouble", "trousers", "true", "turn", "twist", "umbrella", "under", "unit", "value", "verse", "very", "vessel", "view", "violent", "voice", "waiting", "walk", "wall", "warm", "wash", "waste", "watch", "water", "wave", "weather", "week", "weight", "well", "west", "wheel", "when", "where", "while", "whip", "whistle", "white", "wide", "will", "wind", "window", "wine", "wing", "winter", "wire", "wise", "with", "woman", "wood", "wool", "word", "work", "worm", "wound", "writing", "wrong", "year", "yellow", "yesterday", "young"]
 
 
+//generate random number and assign it to an index in dictionary array
+function randomWordIndex(){
+  const guess = Math.floor(Math.random() * dictionary.length)
+   return dictionary[guess]
+}
+const secretWord = randomWordIndex()
 
-function getValidLetterGuess() {
+
+
+//step 3 create an object with key values that will be manipulated
+const gameState = {
+  guessLetters: getUniqueCharacters(), //all correct letters are here
+  remainingGuesses: 10,
+  secretWord: secretWord,
+  display: makeHiddenWord(),
+  playerGuesses: []
+
+}
+
+
+//helper function
   function guessIsValid(letter) {
     return letter.length === 1 && letter.toUpperCase() != letter.toLowerCase()
   }
-  let letter = ""
-  while (!letter) {
-    let input = readline.question("Please enter your guess: ")
+
+
+
+  //gameloop
+  while (gameState.remainingGuesses > 0) {
+    //write the display here so it is updated after everyloop
+    console.log(gameState.display.join(" "))
+    //print the users remaining guess count
+    console.log(gameState.remainingGuesses)
+    //ask for user input
+    const input = readline.question("Please enter your guess: ").toLocaleLowerCase()
     if (guessIsValid(input)) {
-      letter = input
+      //add user guessed letter to playerGuesses array
+      console.log(gameState.playerGuesses.push(input))
+
+      //if user input matches any character in guessLetter array
+      if (gameState.guessLetters.includes(input)) {
+        const letters = gameState.secretWord.split('')
+        for (let i = 0; i < letters.length; i++) {
+          if (letters[i]===input) {
+            gameState.display[i] = input
+          }
+        }
+
+      } else { //if user guesses incorrectly
+        //takes a guess away if wrong
+        gameState.remainingGuesses --
+      }
     } else {
       console.log("Please enter a valid letter")
     }
   }
-  return letter.toLowerCase()
-}
 
-
-let myWord =getValidLetterGuess()
-
-function randomWordIndex(){
-   guess = Math.random() * dictionary.length
-   console.log(dictionary[Math.floor(guess)])
-    word = dictionary[Math.floor(guess)]
-    return word  
-}
-let hiddenWord = randomWordIndex()
-
-const gameState = {
-  guessLetters: [],
-  numberOfGuesses: 10,
-  secretWord:"",
-  display:[]
-
+//create a function that loops through the array secretWord and pushes it into uniqueCharacters to cover all letters that show up in word
+function getUniqueCharacters() {
+  const uniqueCharacters = []
+  const letters = secretWord.split('')
+  for (let i = 0; i < letters.length; i++) {
+    if(!uniqueCharacters.includes(i)) {
+      uniqueCharacters.push(letters[i])
+    }
+  }
+  return uniqueCharacters
 }
 
 //make array of _ that is length of the hidden word
 //convert string to an array of strings
 //iterates through the array giving a new array with _ the same number of times the word has characters
 function makeHiddenWord() {
-  let newArray = hiddenWord.split('')
-  console.log(newArray)
-  let guessArray = []
-  for (let i = 0; i <= newArray.length; i++) {
-    guessArray.push('_')
-    // console.log(guessArray)
-    if(i === newArray.length) {
-      return guessArray
-    }
-    
+  const display =[]
+  for(let i = 0; i < secretWord.length; i++) {
+    display.push('_')
   }
-  return guessArray
+  return display
 }
-makeHiddenWord()
 
-//gameloop
-// function gameLoop () {
-//   while (gameState.numberOfGuesses > 0) {
-  
-
-
-//   }
-// }
-
-
-
-//  let hiddenGameWord = getWord()
-//  let gameWord = getValidLetterGuess()
-// let guessCount = hiddenGameWord.length
-// //  console.log(guessCount)
-//  getWord()
-//  hiddenWord()
-//  getValidLetterGuess()
-// }
-// play()
-
-//  function getWord() {
-//   let myWord = ""
-//    guess = Math.random() * dictionary.length
-//    console.log(dictionary[Math.floor(guess)])
-//     word = dictionary[Math.floor(guess)]
-//     return word
-
-//  }
-//  //i want to make random word an array
-
- 
-
-//  function hiddenWord() {
-//      hidden = []
-//      let str = getWord()
-//      let newStr = str.split('')
-//      for(let i = 0; i < newStr.length; i++){
-//        hidden.unshift('_')
-//        if (i === newStr.length-1){
-//          console.log(hidden)
-//        }
-//       //  console.log(hidden)
-//      }
-//     //  console.log(hidden)
-//    }
-  //  hiddenWord()
-  //  return myWord
-
-// getWord()
-
-// let stats = {
-//   guessedLetters: [],
-//   hiddenWord: [],
-//   numberOfGuesses: 10,
-// }
-
-
-
-// //while (numberOfGuesses)> 0 \\ wordHasBeenFullySolved
-// // keep looping
-
-// const play = () => {
-//   //function that creates a random number that will correspond to the index of dictionary array
-//   
-//     // return dictionary[Math.floor(Math.random() * dictionary.length)]
-//   }
-//   let wordIndex = randomWordIndex(dictionary.length)
-  
-  
-//   //function that returns a random string that user will use to guess
-//   function assignRandomWord(){
-//     return dictionary[wordIndex]
-//   }
-//   let gameWord = assignRandomWord()
-  
-  
-//   //function that makes an array filled with _ that represent a letter of the word
-//   function makeHiddenWord(){
-    
-//     }
-//     // console.log(stats.hiddenWord)
-  
-//   let ghostWord = makeHiddenWord()
-  
-//   //use map to return a new array with each letter changed into a _
-//   function showHiddenWord(){
-//   let hiddenWord = ''
-//   for(let i = 0; i <= gameWord.length; i++) {
-//     if (i ===0){
-//       let hiddenWord = hiddenWord + '_'
-//     } else {
-//       let hiddenWord = hiddenWord + '_'
-//     }
-//   }
-//   return hiddenWord
-//   }
-//   showHiddenWord()
+// main game loop
+// while (numberOfGuesses > 0 || wordHasntBeenFullySolved) {
+//   4. ask user for input readline.question()
+//   5. check if input appears in gameState.guessLetters
+//       5a. If no, decrement numberOfGuesses
+//       5b. If yes, loop through the secret word with a for loop. If 'i' value matches the user guess, then replace the 'i' value in display.
