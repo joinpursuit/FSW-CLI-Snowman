ffa36dd8e5f5c175397d9ac9f2fd20ec7539feff
jcanizales23 <johncanizales@pursuit.org>
2021-01-25 21:36:50 -0500

    Making Progress

--- snowman.js
+++ snowman.js
@@ -4,6 +4,7 @@
 const readlineSync = require('readline-sync');
 const chalk = require('chalk');
 const dictionary = require("./dictionary.js");
+//
 //Intro logs
 console.log("Welcome!");
 console.log("Today we have a game that you may be familiar with.");
@@ -12,16 +13,17 @@ console.log("Yup! I know you've heard of it.");
 console.log("Now let's play!");
 console.log("You have 5 guesses.");
 console.log("HINT! The stars are bright. They lead with might. Look Up!");
+let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
+'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+'t', 'u', 'v', 'w', 'x', 'y', 'z'];
+startGame()
 // gameTakeOff
 const startGame = () => {
   foreach.startGame(randomLetter(pickRandomLetter))
-}
+};
 const randomLetter = () => {
   let pickRandomLetter = readlineSync.question("Pick a letter");
-  let alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h',
-  'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
-  't', 'u', 'v', 'w', 'x', 'y', 'z'];
-  if(pickRandomLetter === randomWord) {
+  if(pickRandomLetter > 0) {
     return true
   } else{
     return false
@@ -37,13 +39,18 @@ const randomWord = () => {
 };
 // numberOfGuess
 const numOfGuess = () => {
+  while(numOfGuesses > 0) {
+    console.log("You are out of Guesses")
+    if(numOfGuess != null && numOfGuess != "") {
+      numOfGuesses -= numOfGuess
 
+    }
+  }
+  console.log("You are out of guesses")
 };
 // guessLog
 const guessLog = () => {
 
 };
 // gameOver
-const gameOver = () => {
-
-};
+const gameOver = () => {}
