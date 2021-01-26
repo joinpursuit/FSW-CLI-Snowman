d27f8e69996d47938cad81849d229b1f3f78c07f
Geormary Sanchez <geormarysanchez.gs@gmail.com>
2021-01-25 18:13:48 -0800

    completed the game

--- snowman.js
+++ snowman.js
@@ -2,8 +2,9 @@ const readline = require("readline-sync");
 const dictionary = require("./dictionary");
 let gameState = {guess: 7, lettersGuessed: [], word: "", board:[]};
 function playOrNot(){
-  console.log("Welcome message and game instructions:")
-  let play = readline.keyInYN(`Would you like to play?`)
+  console.clear()
+  console.log("\nWelcome to the Snowman Game! \nTo play, I will choose a word and allow you 7 guesses to guess the right word. \nThroughout the game, you'll be able to see: how many letters are in the word; how many guesses you have left; and what letters you have already guessed.\n")
+  let play = readline.keyInYN(`Do you wanna build a Snowman?`)
   if(play){
     startGame()
   } else{
@@ -12,7 +13,7 @@ function playOrNot(){
 }
 const startGame = () => {
   console.clear()
-  console.log("Let's put this snowman together");
+  console.log("\nLet's put this snowman together\n");
   gameState.word = randomWord();
   gameState.board = createBoard(gameState.word.length);
   gameLoop();
@@ -45,9 +46,10 @@ function createBoard(num) {
   return board;
 }
 function logGameState(){
+  console.clear()
   console.log(gameState.board.join(" "))
-  console.log(`You've guessed: ${gameState.lettersGuessed}`);
-  console.log(`You have ${gameState.guess} remaining guesses`);
+  console.log(`\nYou've guessed: ${gameState.lettersGuessed}`);
+  console.log(`\nYou have ${gameState.guess} remaining guesses`);
 }
 function updateGameState(guess) {
   gameState.lettersGuessed.push(guess)
@@ -72,7 +74,6 @@ function getValidLetterGuess() {
   }
   return letter.toLowerCase();
 }
-//add dash and if/else it includes then add letter then add dashes again
 function updateBoard(guess){
   for (let i = 0; i < gameState.word.length; i++) {
    if(gameState.word[i] === guess) {
@@ -81,26 +82,37 @@ function updateBoard(guess){
   }
 
 }
-const endGame = () => {};
+const endGame = () => {
+  console.log('Goodbye')
+  process.exit()
+};
 const winner = () => {
   console.log(
-    `WOW! You won, and it only took you ${gameState.lettersGuessed.length} guesses! You got it right, the word is indeed ${gameState.word}!`
+    `\nWOW! You won, and it only took you ${gameState.lettersGuessed.length} guesses! You got it right, the word is indeed: ${(gameState.word).toUpperCase()}!`
   );
+  playAgain()
 };
 const playAgain = () => {
-  console.log();
-  let answer = readline.keyInYN(`Would you like to try again`);
+  clearState()
+  let answer = readline.keyInYN(`Would you like to build another snowman?`);
   if (answer) {
-    gameLoop();
-  } else endGame();
+    startGame();
+  } else{
+    endGame();
+  }
 };
 const loser = () => {
   if (gameState.guess === 0) {
-    console.log(
-      `GAME OVER. You lost, you have no more guesses left. The word was ${gameState.word}.`
+    console.log(`\nGAME OVER. You lost, you have no more guesses left. The word was: ${(gameState.word).toUpperCase()}.`
     );
   }
+  playAgain()
 };
+function clearState(){
+  gameState.word= ''
+  gameState.board = []
+  gameState.lettersGuessed = []
+}
 
 playOrNot();
 
