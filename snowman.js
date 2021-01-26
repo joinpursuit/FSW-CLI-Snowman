6d0d2084b028d54017cbc92da382e487f509af37
Auredy Sanchez <auredysanchez@gmail.com>
2021-01-25 20:33:11 -0800

    OMG I Finished

--- snowman.js
+++ snowman.js
@@ -1,35 +1,35 @@
 const dictionary = require("./dictionary");
 const readline = require("readline-sync");
 // i is always a number not the element in the array. We use that number to key in to the array
-
-// let mistakes = 0;
-let guessed = [];
  
-let stats = { dash: [], guesses: 2, guessed: [], word: "", board: "" };
+let stats = { dash: [], guesses: 7, guessed: [], word: "", board: "" };
 
 function startGame() {
+  console.clear()
   console.log("Welcome to Snowman!!\n");
   console.log("A game where you have to guess the word I'm thinking");
-  console.log(`"You have ${stats.guesses} guesses to figure out the word"`);
-  console.log("Are you brave enough?\n");
-  let input = readline.keyInYN("Do you want to play?\n");
+  console.log(`You have ${stats.guesses} guesses to figure out the word.\n`);
+  let input = readline.keyInYN("Are you brave enough?\n");
   if (input) {
     randomWord();
   } else {
     leaveGame();
   }
-  console.log(stats.word);
+  
   wordDashes(stats.word);
   while (stats.guesses > 0) {
-    //|| stats.dash.includes("_")
+    console.clear()
     if (!stats.dash.includes("_")) {
-      console.log("You won!");
-      console.log(`"You made a total of ${stats.guessed.length} guesses"`)
+      console.log("You won!\n");
+      console.log(`Woot woot!! You got the right word! The word is ${stats.word}!\n`)
+      console.log(`You made a total of ${stats.guessed.length} guesses.\n`)
       resetGame();
       break;
     }
     console.log(stats.guessed.join(" "))
-    console.log(`"You have ${stats.guesses} guesses remaining"`);
+    console.log(stats.word);
+    console.log(`You have ${stats.guesses} guesses remaining`
+    );
     console.log(stats.dash.join(" "));
     stats.board = getValidLetterGuess();
     stats.guessed.push(stats.board)
@@ -39,14 +39,16 @@ function startGame() {
       letterHolder(stats.board);
     }
   }
-
+  console.clear()
   lostGame();
- 
+
 }
 function resetGame() {
   let input = readline.keyInYN("Do you want to play again?\n");
+  console.clear()
   if (input) {
     stats = { dash: [], guesses: 7, guessed: [], word: "", board: "" };
+    console.clear()
     startGame();
   } else {
     leaveGame();
@@ -54,8 +56,8 @@ function resetGame() {
 }
 
 function lostGame() {
-  console.log(`The word you were trying to guess was: ${stats.word}\n`)
-  console.log("Boo you lost. Want to try again?\n");
+  console.log("\nBoo you lost!\n");
+  console.log(`The word you were trying to guess was: ${stats.word}!\n`)
   resetGame();
 }
 
@@ -64,23 +66,13 @@ function randomWord() {
   return stats.word;
 }
 
-function wordDashes() {
-  // dashes on the board
-  // builds board
+function wordDashes() {// dashes on the board//builds board
   for (let i = 0; i < stats.word.length; i++) {
     stats.dash.push("_");
   }
   return stats.dash;
 }
-// console.log(wordDashes)
-
-// function guessedOut(letter) {
-//   if (stats.word.includes(stats.board)) {
-//   }
-// }
-
-function letterHolder(letter) {
-  //update the dash with the letter on the board
+function letterHolder(letter) { //update the dash with the letter on the board
   for (let i = 0; i < stats.word.length; i++) {
     if (stats.word[i] === letter) {
       stats.dash[i] = letter;
@@ -97,7 +89,6 @@ function getValidLetterGuess() {
     let input = readline.question("Please enter your guess: ");
     if (guessIsValid(input)) {
       letter = input;
-      // updateBoard(letter);
     } else {
       ``;
       console.log("Please enter a valid letter");
@@ -106,50 +97,12 @@ function getValidLetterGuess() {
   return letter.toLowerCase();
 }
 
-// function updateBoard(){
-
-//   if(letter[i] === word){
-//         dash+=1
-//   } else {
-//     guesses--
-//   }
-// }
-
-// function updateBoard() {
-//   for (let i = 0; i < word.length; i++) {
-//     if (word[i] === letter.toLowerCase()) {
-//      dash[i] = letter.toLowerCase();
-//     }
-//   }
-//   // dash = spaces.join(" ");
-//   return dash
-// }
-
-// function nextGuess() {
-//   let guesses = 7
-//   while(remainingGuesses > 0){
-//     let input = readline.question("Please enter your guess: ");
-
-//     checkLetter()
-//   }
-// }
-// while(remainingGuesses > 0){
-//if the guess is right
-//want to check if the guesses is right. fill in dashes
-// }
-// if(guesses === 0){
-
-// }
-
-//outside of whileloop "no  remainingGuesses"
-// remainingGuesses--
-
 function leaveGame() {
+  console.clear()
   console.log(
     "\nAw man! I guess you weren't brave enough to handle the challenge"
   );
   console.log("Not sad to see you go!");
-  // resetGame()
   process.exit();
 }
 startGame();
