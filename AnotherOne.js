80a8fae7f640c2b5efbcc58021512d97c09a0f99
Miguel Garcia <miguelgarcia@pursuit.org>
2021-01-26 20:55:30 -0500

    adding replay, not fully functional

--- AnotherOne.js
+++ AnotherOne.js
@@ -3,6 +3,8 @@ const readline = require("readline-sync");
 const dictionary = require("./dictionary")
 
 
+
+
 let obj = {
   guessesRemaining: 9,
   correctGuesses: [],
@@ -10,6 +12,11 @@ let obj = {
   secretWord: "",
   board: [],
 };
+
+let gR = obj.guessesRemaining
+let cG = obj.correctGuesses
+let iG = obj.incorrectGuesses
+let bD = obj.board
 
 
 const randomWord = () => {
@@ -48,13 +55,22 @@ const getValidLetterGuess = () => {
 let x = ""
 
 const theEnd = () => {
-  if ((obj.guessesRemaining === 0)) {
+  if ((gR === 0)) {
     console.log("Better luck next time.");
     console.log("The word was " + obj.secretWord.toUpperCase() + ".");
   } else if (obj.secretWord === obj.board.join("")) {
     console.log("You win! Congratulations!");
   }
+  if (readline.keyInYNStrict("Would you like to play again? ")) {
+    console.clear();
+    return startGame();
+  }
+  else {
+    process.exit();
+  }
 };
+
+
 
 //isGameOver function to return boolean if game is over
 
@@ -68,45 +84,63 @@ const wordCompare = () => {
 const boardUpdate = () => {
     for (let j = 0; j < obj.secretWord.length; j++) {
         if (obj.secretWord[j] === x) {
-          // console.log(obj.secretWord)
             obj.board[j] = x
         }
     }
     return obj.board
 }
 
-// const isGameOver = () => {
-//   if (obj.guessesRemaining === 0 || obj.board.join("") !== obj.secretWord)
-//     return true
-// }
 
+
+const duplicateLetter = () => {
+  for (let z = 0; z < cG.length; z++) {
+    while (x === cG[z]) {
+      console.log("That letter was already guessed. Please try another.")
+      getValidLetterGuess()
+    }
+  }
+  for (let z = 0; z < iG.length; z++) {
+    while (x === iG[z]) {
+      console.log("That letter was already guessed. Please try another.")
+      getValidLetterGuess()
+    }
+  }
+}
+
+
+// const welcome = () => {
+//   console.log(" Welcome to the Snowman Game.\n Try to predict the word by guessing individual letters.\n Good luck!\n")
+// }
+let welcome = readline.question(" Welcome to the Snowman Game.\n Try to predict the word by guessing individual letters.\n Good luck!\n")
 
 const startGame = () => {
-    obj.secretWord = randomWord();
+  welcome
+  obj.secretWord = randomWord();
     buildBoard().join(" ")
-    // console.clear();
-  while (obj.guessesRemaining > 0 && obj.board.join("") !== obj.secretWord) {
-    getValidLetterGuess();                      
+    // welcome();
+  while (gR > 0 && bD.join("") !== obj.secretWord) {
+    console.clear();
+    console.log(`Guesses Remaining: ${gR}\n Correct Guesses : ${cG}\n Incorrect Guesses: ${iG}\n ${bD.join(" ")}\n`, obj.secretWord);
+    getValidLetterGuess();
+    duplicateLetter();                      
     if (wordCompare()) {
       console.log("You guessed right!");
       boardUpdate()
-      obj.correctGuesses.push(x);
-      console.log(obj);
+      cG.push(x);
     } else if (!wordCompare()) {
       console.log("Wrong guess.");
-      console.log(boardUpdate())
-      obj.guessesRemaining -= 1;
-      obj.incorrectGuesses.push(x);
-      console.log(obj);
+      gR -= 1;
+      iG.push(x);
     }
   }
 };
 
 
+
+
+
 startGame()
 theEnd()
 
-// console.log("a", buildBoard());
-// console.log("b", obj);
-// getValidLetterGuess();
-// updater();
+
+
